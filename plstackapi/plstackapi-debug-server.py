e02d72003b525bbc060b9e95a8782caec093bd0b
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-03-29 11:23:56 -0400

    fix config options

--- plstackapi/plstackapi-debug-server.py
+++ plstackapi/plstackapi-debug-server.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plstackapi.planetstack.settings")
     from django.core.management import ManagementUtility
     config = Config()
-    url = "%s:%s" % (config.nova_api_host, config.nova_api_port)
+    url = "%s:%s" % (config.api_host, config.api_port)
     args = [__file__, 'runserver', url] 
     server = ManagementUtility(args)
     server.execute()
