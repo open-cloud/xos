67f60ce91ae5c243d9ad2ea094ac6abf779fcefc
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-02 10:46:07 -0400

    site name is not unique

--- plstackapi/core/models.py
+++ plstackapi/core/models.py
@@ -14,7 +14,7 @@ class PlCoreBase(models.Model):
 
 class Site(PlCoreBase):
     tenant_id = models.CharField(max_length=200, help_text="Keystone tenant id")
-    name = models.CharField(max_length=200, unique=True, help_text="Name for this Site")
+    name = models.CharField(max_length=200, help_text="Name for this Site")
     site_url = models.URLField(help_text="Site's Home URL Page", blank=True)
     enabled = models.BooleanField(default=True, help_text="Status for this Site")
     longitude = models.FloatField(null=True, blank=True)
