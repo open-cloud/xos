f2162d9aa6a016655c55ef7ac29275f03c1ee8e5
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-07 19:32:20 -0400

    added role_id to role model

--- plstackapi/planetstack/models.py
+++ plstackapi/planetstack/models.py
@@ -21,6 +21,7 @@ class PlCoreBase(models.Model):
 class Role(PlCoreBase):
 
     ROLE_CHOICES = (('admin', 'Admin'), ('pi', 'Principle Investigator'), ('user','User'))
+    role_id = models.CharField(max_length=256, unique=True)
     role_type = models.CharField(max_length=80, unique=True, choices=ROLE_CHOICES)
 
     def __unicode__(self):  return u'%s' % (self.role_type)
