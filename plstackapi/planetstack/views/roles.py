9f2379721b2cb92f2bd035402d6fc318696f1099
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-07 23:43:11 -0400

    implement RoleListCreate

--- plstackapi/planetstack/views/roles.py
+++ plstackapi/planetstack/views/roles.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from plstackapi.planetstack.models import Role
+from plstackapi.planetstack.api.roles import add_role, delete_role, get_roles
+from plstackapi.planetstack.serializers import RoleSerializer
 
 
 class RoleListCreate(APIView):
@@ -12,5 +13,20 @@ class RoleListCreate(APIView):
     """
 
     def post(self, request, format = None):
-        print request
+        
+        if 'auth' not in request.DATA
+            return Response(status=status.HTTP_400_BAD_REQUEST)        
+        else if 'name' in request.DATA:
+            role = add_role(request.DATA['auth'], request.DATA['name'])
+            serializer = RoleSerializer(data=role)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            roles = get_roles(request.DATA['auth'])
+            serializer = RoleSerializer(roles, many=True)
+            return Response(Serializer.data)
+        
+            
+        
+            
+            
         
