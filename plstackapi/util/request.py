82aa4f07c8f7c8d5d7a004003f27295fc073df72
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-08 17:45:42 -0400

    request may be json string or djagno QueryDict

--- plstackapi/util/request.py
+++ plstackapi/util/request.py
@@ -1,7 +1,11 @@
 import ast
+from django.http.request import QueryDict
 
 def parse_request(request):
     d = {}
-    for (k,v) in request.items():
-        d[k] = ast.literal_eval(v) 
+    if isinstance(request, unicode):
+        d = ast.literal_eval(request)
+    elif isinstance(request, QueryDict):
+        for (k,v) in request.items():
+            d[k] = ast.literal_eval(v)
     return d
