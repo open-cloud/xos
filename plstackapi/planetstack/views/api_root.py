6b3ab0cf35f20168d072d1df30dd8c4360385209
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-07 23:42:44 -0400

    disable paths

--- plstackapi/planetstack/views/api_root.py
+++ plstackapi/planetstack/views/api_root.py
@@ -5,8 +5,9 @@ from rest_framework.reverse import reverse
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'nodes': reverse('node-list', request=request, format=format),
-        'sites': reverse('site-list', request=request, format=format),
-        'deploymentNetworks': reverse('deploymentnetwork-list', request=request, format=format),
-        'slices': reverse('slice-list', request=request, format=format)
+        'roles': reverse('role-list', request=request, format=format),
+        #'nodes': reverse('node-list', request=request, format=format),
+        #'sites': reverse('site-list', request=request, format=format),
+        #'deploymentNetworks': reverse('deploymentnetwork-list', request=request, format=format),
+        #'slices': reverse('slice-list', request=request, format=format)
     })
