9b34afb1507f8a57b395b3eaffacc434be71ae3f
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-07 20:32:11 -0400

    fix paths

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 setup(name='plstackapi',
       version='0.1',
       description='PlanetStack API',
-      packages=['plstackapi', 'plstackapi/planetstack', 'plstackapi/planetstack/views', 'plstackapi/planetstack/api', 'plstackapi/openstack' ,'plstackapi/util', 'plstackapi/importer', 'plstackapi/importer/plclassic'],
+      packages=['plstackapi', 'plstackapi/planetstack', 'plstackapi/planetstack/views', 'plstackapi/planetstack/api', 'plstackapi/planetstack/fixtures', 'plstackapi/openstack' ,'plstackapi/util', 'plstackapi/importer', 'plstackapi/importer/plclassic'],
       scripts=['plstackapi/plstackapi-debug-server.py'],
       data_files=[
         ('/etc/planetstack/', ['config/plstackapi_config']),
