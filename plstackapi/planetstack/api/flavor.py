aae0bf0a38a5751fd3157f449acc166039eea765
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-09 09:43:50 -0400

    initial checkin

---
+++ plstackapi/planetstack/api/flavor.py
@@ -0,0 +1,21 @@
+from plstackapi.openstack.client import OpenStackClient
+from plstackapi.openstack.driver import OpenStackDriver
+from plstackapi.planetstack.api.auth import auth_check
+from plstackapi.planetstack.models import Flavor
+ 
+
+def add_flavor(auth, fields={}):
+    """not implemented"""
+    return 
+
+def delete_flavor(auth, filter={}):
+    """not implemented"""
+    return 1
+
+def get_flavors(auth, filter={}):
+    auth_check(auth)   
+    flavors = Flavor.objects.filter(**filter)
+    return flavors             
+        
+
+    
