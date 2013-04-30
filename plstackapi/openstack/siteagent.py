ea4de1a217286044ec4eb1742fb01f8b3dba4881
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-04-30 19:07:45 -0400

    fix bugs

--- plstackapi/openstack/siteagent.py
+++ plstackapi/openstack/siteagent.py
@@ -8,10 +8,12 @@ class SiteAgent:
     def run(self):
         driver = OpenStackDriver()
         # fill in null tenant ids 
-        sites = Site.objects.filter(tenant_id=None)
+        sites = Site.objects.filter(tenant_id__in=[None, ''])
         for site in sites:
             # calling save() on the model should force the tenant_id to be set
             site.driver = driver
+            site.caller = driver.admin_user
+            site.caller.user_id = site.caller.id
             site.save() 
                                         
 if __name__ == '__main__':
