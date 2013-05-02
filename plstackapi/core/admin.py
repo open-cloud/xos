f9f4afb2ef1c82e4fcea2380aadc7d659d58017c
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-05-01 21:02:12 -0400

    call user.set_password() in PLUser.save() instead of PLUserAdmin.save()

--- plstackapi/core/admin.py
+++ plstackapi/core/admin.py
@@ -280,7 +280,8 @@ class UserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.password = self.cleaned_data["password1"]
+        #user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
