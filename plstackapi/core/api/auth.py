b30ac0582534c294cacb3599a96ce1960d637cf1
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-05-06 16:58:21 -0400

    removing

--- plstackapi/core/api/auth.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import authenticate
-from plstackapi.openstack.manager import OpenStackManager
-
-def auth_check(username, password, tenant):
-    client = OpenStackClient(username=username,
-                             password=password,
-                             tenant=tenant)
-    client.authenticate()
-    return client
