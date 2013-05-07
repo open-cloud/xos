276fa80d2b07d479b4d3ef0013b87860b32f0171
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-05-07 15:18:16 -0400

    add core/models to packages path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from plstackapi.util.glob import recursive_glob
 setup(name='plstackapi',
       version='0.1',
       description='PlanetStack API',
-      packages=['plstackapi', 'plstackapi/planetstack', 'plstackapi/core', 'plstackapi/core/views', 'plstackapi/core/api', 'plstackapi/core/fixtures', 'plstackapi/openstack' ,'plstackapi/util', 'plstackapi/importer', 'plstackapi/importer/plclassic'],
+      packages=['plstackapi', 'plstackapi/planetstack', 'plstackapi/core','plstackapi/core/models' , 'plstackapi/core/views', 'plstackapi/core/api', 'plstackapi/core/fixtures', 'plstackapi/openstack' ,'plstackapi/util', 'plstackapi/importer', 'plstackapi/importer/plclassic'],
       scripts=['plstackapi/plstackapi-debug-server.py'],
       data_files=[
         ('/etc/planetstack/', ['config/plstackapi_config']),
