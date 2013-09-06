e7d8814e09c3876a356811c675d4fd1fb1f2cfbe
Siobhan Tully <stully@verivue.com>
2013-09-06 11:02:11 -0400

    Deprecated Members model

--- planetstack/core/models/deployment.py
+++ planetstack/core/models/deployment.py
@@ -1,14 +1,12 @@
 import os
 from django.db import models
 from core.models import PlCoreBase
-from core.models import Member
 from django.contrib.contenttypes import generic
 
 # Create your models here.
 
 class Deployment(PlCoreBase):
     name = models.CharField(max_length=200, unique=True, help_text="Name of the Deployment")
-    members = generic.GenericRelation(Member)
 
     def __unicode__(self):  return u'%s' % (self.name)
 
