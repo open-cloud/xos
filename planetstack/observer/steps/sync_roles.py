03742a763cae633fc660bf3bb55b71a1ad2dec6a
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-10-08 14:42:21 -0400

    fix syntax error

--- planetstack/observer/steps/sync_roles.py
+++ planetstack/observer/steps/sync_roles.py
@@ -13,7 +13,7 @@ class SyncRoles(OpenStackSyncStep):
 
     def fetch_pending(self):
         site_roles = SiteRole.objects.filter(Q(enacted__lt=F('updated')) | Q(enacted=None)
-        slice_roles = SliceRole.objects.filter(Q(enacted__lt=F('updated') | Q(enacted=None))
+        slice_roles = SliceRole.objects.filter(Q(enacted__lt=F('updated')) | Q(enacted=None))
 
         roles = []
         for site_role in site_roles:
