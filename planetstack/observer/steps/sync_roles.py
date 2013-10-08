f4f1616ef3bd5dbfda6dcb77cfe64380ece883e8
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-10-08 15:30:35 -0400

    include DeploymentRole in role sync

--- planetstack/observer/steps/sync_roles.py
+++ planetstack/observer/steps/sync_roles.py
@@ -6,6 +6,7 @@ from observer.openstacksyncstep import OpenStackSyncStep
 from core.models.role import Role
 from core.models.site import SiteRole
 from core.models.slice import SliceRole
+from core.models.deployment import DeploymentRole
 
 class SyncRoles(OpenStackSyncStep):
     provides=[Role]
@@ -14,12 +15,15 @@ class SyncRoles(OpenStackSyncStep):
     def fetch_pending(self):
         site_roles = SiteRole.objects.filter(Q(enacted__lt=F('updated')) | Q(enacted=None)
         slice_roles = SliceRole.objects.filter(Q(enacted__lt=F('updated')) | Q(enacted=None))
+        deployment_roles = DeploymentRole.objects.filter(Q(enacted__lt=F('updated')) | Q(enacted=None))
 
         roles = []
         for site_role in site_roles:
             roles.append(site_role)
         for slice_role in slice_roles:
             roles.append(slice_role)
+        for deployment_role in deployment_roles:
+            roles.append(deployment_role)
 
         return roles
 
