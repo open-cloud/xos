3bbf5ed2faffb4eb4b23df0ba659f1ab82a83549
Sapan Bhatia <gwsapan@gmail.com>
2014-01-13 13:29:12 -0500

    First version of api generator

---
+++ planetstack/apigen/api.template.py
@@ -0,0 +1,63 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import serializers
+from core.models import *
+from django.forms import widgets
+
+"""
+	Schema of the generator object:
+		all: Set of all Model objects
+		all_if(regex): Set of Model objects that match regex
+	
+	Model object:
+		plural: English plural of object name
+		camel: CamelCase version of object name
+		refs: list of references to other Model objects
+
+	TODO: Deal with subnets
+"""
+
+# Based on api_root.py
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+		{% for object in generator.all %} 
+        '{{ object.plural }}': reverse('{{ object }}-list', request=request, format=format),
+		{% endfor %}
+    })
+
+# Based on serializers.py
+
+{% for object in generator.all %}
+
+class {{ object.camel }}Serializer(serializers.HyperlinkedModelSerializer):
+	id = serializers.Field()
+	{% for ref in object.refs %}
+	sites = serializers.HyperlinkedRelatedField(view_name='{{ ref }}-detail')
+	{% endfor %}
+	class Meta:
+		model = {{ object }}
+		fields = ('id',{% for ref in object.refs %}'{{ ref }}',{% endfor %})
+{% endfor %}
+
+serializerLookUp = { 
+{% for object in generator.all %}
+                 {{ object.camel }}: {{ object.camel }}Serializer,
+{% endfor %}
+                 None: None,
+                }
+
+# Based on core/views/*.py
+{% for object in generator.all %}
+
+class {{ object.camel }}List(generics.ListCreateAPIView):
+    queryset = {{ object.camel }}.objects.all()
+    serializer_class = {{ object.camel }}Serializer
+
+class {{ object.camel }}Detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = {{ object.camel }}.objects.all()
+    serializer_class = {{ object.camel }}Serializer
+
+{% endfor %}
