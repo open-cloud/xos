49c10f5affeaf196045199975d7c3e4900207d88
Tony Mack <tmack@paris.CS.Princeton.EDU>
2014-04-29 12:25:57 -0400

    disable unused steps

--- planetstack/observer/steps/__init__.py
+++ planetstack/observer/steps/__init__.py
@@ -1,4 +1,4 @@
-from .sync_external_routes import SyncExternalRoutes
+#from .sync_external_routes import SyncExternalRoutes
 from .sync_network_slivers import SyncNetworkSlivers
 from .sync_networks import SyncNetworks
 from .sync_network_deployments import SyncNetworkDeployments
@@ -6,7 +6,7 @@ from .sync_site_privileges import SyncSitePrivileges
 from .sync_sites import SyncSites
 from .sync_slice_memberships import SyncSliceMemberships
 from .sync_slices import SyncSlices
-from .sync_sliver_ips import SyncSliverIps
+#from .sync_sliver_ips import SyncSliverIps
 from .sync_slivers import SyncSlivers
 from .sync_users import SyncUsers
 from .sync_roles import SyncRoles
