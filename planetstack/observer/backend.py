b4cf100663ae9f251fe42d4dd1708041c1d3d088
Tony Mack <tmack@paris.CS.Princeton.EDU>
2014-04-29 12:26:12 -0400

    modifed

--- planetstack/observer/backend.py
+++ planetstack/observer/backend.py
@@ -1,17 +1,24 @@
 import threading
+import time
 from observer.event_loop import PlanetStackObserver
 from observer.event_manager import EventListener
+from util.logger import Logger, logging
+
+logger = Logger(level=logging.INFO)
 
 class Backend:
     
     def run(self):
-        # start the openstack observer
-        observer = PlanetStackObserver()
-        observer_thread = threading.Thread(target=observer.run)
-        observer_thread.start()
+        try:
+            # start the openstack observer
+            observer = PlanetStackObserver()
+            observer_thread = threading.Thread(target=observer.run)
+            observer_thread.start()
+            
+            # start event listene
+            event_manager = EventListener(wake_up=observer.wake_up)
+            event_manager_thread = threading.Thread(target=event_manager.run)
+            event_manager_thread.start()
+        except:
+            logger.log_exc("Exception in child thread")
 
-        # start event listene
-        event_manager = EventListener(wake_up=observer.wake_up)
-        event_manager_thread = threading.Thread(target=event_manager.run)
-        event_manager_thread.start()
-                
