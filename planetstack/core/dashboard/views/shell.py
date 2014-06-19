0b48b4b7f6551d92eb79776621e2f36a91bee073
Scott Baker <smbaker@gmail.com>
2014-06-19 15:24:58 -0700

    fix datetime not serializable in shell view

--- planetstack/core/dashboard/views/shell.py
+++ planetstack/core/dashboard/views/shell.py
@@ -1,11 +1,23 @@
 # /opt/planetstack/core/dashboard/views/helloworld.py
+import datetime
 import os
 import sys
+import time
 import json
 from django.http import HttpResponse, HttpResponseServerError, HttpResponseForbidden
 from django.views.generic import TemplateView, View
 from core.models import *
 from django.forms.models import model_to_dict
+
+def ensure_serializable(d):
+    d2={}
+    for (k,v) in d.items():
+        # datetime is not json serializable
+        if isinstance(v, datetime.datetime):
+            d2[k] = time.mktime(v.timetuple())
+        else:
+            d2[k] = v
+    return d2
 
 class ShellDataView(View):
     url = r'^shelldata/'
@@ -13,7 +25,7 @@ class ShellDataView(View):
     def get(self, request, **kwargs):
         allSlices = []
         for slice in Slice.objects.all():
-            allSlices.append(model_to_dict(slice))
+            allSlices.append(ensure_serializable(model_to_dict(slice)))
 
         result = {"slices": allSlices}
 
