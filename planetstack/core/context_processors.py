5face3a51e012b741a48302207fc1b025e0909ca
Scott Baker <smbaker@gmail.com>
2014-08-05 10:15:26 -0700

    dynamically get list of sites for request account page

--- planetstack/core/context_processors.py
+++ planetstack/core/context_processors.py
@@ -1,4 +1,14 @@
 from django.conf import settings
+from core.models import Site
+
 
 def planetstack(request):
-    return {"DISABLE_MINIDASHBOARD": settings.DISABLE_MINIDASHBOARD}
+    allSites = []
+    for site in Site.objects.all():
+        allowNewUsers = True    # replace with logic for blessing sites for registration, if necessary
+        allSites.append( {"name": site.name,
+                           "id": site.id,
+                           "allowNewUsers": allowNewUsers} )
+
+    return {"DISABLE_MINIDASHBOARD": settings.DISABLE_MINIDASHBOARD,
+            "sites": allSites}
