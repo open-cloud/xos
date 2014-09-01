31138dd47806d0c2aa2c659ed6a01185b252f475
Sapan Bhatia <gwsapan@gmail.com>
2014-09-01 01:45:28 -0400

    Added DeploymentCredential to set of supported credentials

--- planetstack/core/models/credential.py
+++ planetstack/core/models/credential.py
@@ -36,3 +36,14 @@ class SliceCredential(PlCoreBase):
 
     def __unicode__(self):
         return self.name
+
+class DeploymentCredential(PlCoreBase):
+    deployment = models.ForeignKey(Deployment, related_name='credentials', help_text="The User this credential is associated with")
+
+    name = models.SlugField(help_text="The credential type, e.g. ec2", max_length=128)
+    key_id = models.CharField(help_text="The backend id of this credential", max_length=1024)
+    enc_value = EncryptedCharField(help_text="The key value of this credential", max_length=1024)
+
+
+    def __unicode__(self):
+        return self.name
