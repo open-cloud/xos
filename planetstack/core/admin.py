ec23b99bb293e15eeae1c181fe8142f77cad81c1
Tony Mack <tmack@tux.cs.princeton.edu>
2014-09-02 21:18:45 -0400

    merge

--- planetstack/core/admin.py
+++ planetstack/core/admin.py
@@ -762,20 +762,19 @@ class SliceAdmin(PlanetStackBaseAdmin):
             for deployment in flavor.deployments.all():
                 deployment_flavors.append( (deployment.id, flavor.id, flavor.name) )
 
-        sites = {}
+        site_login_bases = []
         for site in Site.objects.all():
-            sites[site.id] = site.login_base
-
+            site_login_bases.append((site.id, site.login_base)) 
+        
         context["deployment_nodes"] = deployment_nodes
         context["deployment_flavors"] = deployment_flavors
-        context["sites"] = sites
-
+        context["site_login_bases"] = site_login_bases
         return super(SliceAdmin, self).render_change_form(request, context, add, change, form_url, obj)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'site':
             kwargs['queryset'] = Site.select_by_user(request.user)
-            kwargs['widget'] = forms.Select(attrs={'onChange': "update_slice_name(this, $($(this).closest('div')[0]).find('.field-name input')[0].id)"})
+            kwargs['widget'] = forms.Select(attrs={'onChange': "update_slice_prefix(this, $($(this).closest('fieldset')[0]).find('.field-name input')[0].id)"})
 
         return super(SliceAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
