6067faa4c226fde1b4444b71fccd032c54a531e3
Sapan Bhatia <gwsapan@gmail.com>
2014-09-04 00:38:31 -0400

    Moved out rogue file 'model_policies.py' which was interfering with the import of the model_policies directory

--- planetstack/model_policies.py
+++
@@ -1,8 +0,0 @@
-from django.core.signals import post_save
-from django.dispatch import receiver
-import pdb
-
-@receiver(post_save)
-def post_save_handler(sender, **kwargs):
-	pdb.set_trace()
-    print("Request finished!")
