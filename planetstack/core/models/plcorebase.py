3d2d3e46d33967414833fc93f3420787a1cd0c08
Scott Baker <smbaker@gmail.com>
2014-10-09 16:22:00 -0700

    fix keyerror in plcorebase.delete

--- planetstack/core/models/plcorebase.py
+++ planetstack/core/models/plcorebase.py
@@ -117,14 +117,17 @@ class PlCoreBase(models.Model, DiffModelMixIn):
     def delete(self, *args, **kwds):
         # so we have something to give the observer
         purge = kwds.get('purge',False)
+        if purge:
+            del kwds['purge']
         silent = kwds.get('silent',False)
+        if silent:
+            del kwds['silent']
         try:
             purge = purge or observer_disabled
         except NameError:
             pass
 
         if (purge):
-            del kwds['purge']
             super(PlCoreBase, self).delete(*args, **kwds)
         else:
             self.deleted = True
