3df41943dceeba4fd23263cd84d35468b79e4915
Scott Baker <smbaker@gmail.com>
2014-10-28 12:44:13 -0700

    detail view

--- planetstack/core/xoslib/static/js/test.js
+++ planetstack/core/xoslib/static/js/test.js
@@ -10,7 +10,8 @@ TestApp.addRegions({
     siteList: "#siteList",
     sliceList: "#sliceList",
     sliverList: "#sliverList",
-    userList: "#userList"
+    userList: "#userList",
+    detail: "#detail"
 });
 
 // ---- Deployment ----
@@ -38,13 +39,52 @@ TestApp.on("start", function() {
          name = objs[index];
          tr_template = '#test-' + name + '-listitem-template';
          table_template = '#test-' + name + '-list-template';
+         detail_template = '#test-' + name + '-detail-template';
          collection_name = name + "s";
          region_name = name + "List";
 
+         detailClass = Marionette.ItemView.extend({
+            template: detail_template,
+            tagName: 'div',
+
+            events: {"click button.js-submit": "submitClicked",
+                     "change input": "inputChanged"},
+
+            /* inputChanged is watching the onChange events of the input controls. We
+               do this to track when this view is 'dirty', so we can throw up a warning
+               if the user tries to change his slices without saving first.
+            */
+
+            inputChanged: function(e) {
+                this.dirty = true;
+            },
+
+            submitClicked: function(e) {
+                e.preventDefault();
+                var data = Backbone.Syphon.serialize(this);
+                this.model.save(data);
+                this.dirty = false;
+            },
+         });
+
          itemViewClass = Marionette.ItemView.extend({
+             detailClass: detailClass,
              template: tr_template,
              tagName: 'tr',
              className: 'test-tablerow',
+
+             events: {"click": "changeItem"},
+
+             changeItem: function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    var detailView = new this.detailClass({
+                        model: this.model,
+                    });
+                    $('#detailBox').show();
+                    TestApp.detail.show(detailView);
+              },
          });
 
          listViewClass = Marionette.CompositeView.extend({
@@ -63,6 +103,12 @@ TestApp.on("start", function() {
          TestApp[region_name].show(listView);
          xos[collection_name].startPolling();
      }
+
+     $('#close-detail-view').bind('click', function() {
+         $('#detailBox').hide();
+     });
+
+     $('#detailBox').hide();
 });
 
 $(document).ready(function(){
