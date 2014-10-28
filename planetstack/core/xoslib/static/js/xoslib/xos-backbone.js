5c92ca900779a69ef7cd2df4ec16f4bc71d45565
Scott Baker <smbaker@gmail.com>
2014-10-28 15:57:40 -0700

    CSRF token support in xoslib

--- planetstack/core/xoslib/static/js/xoslib/xos-backbone.js
+++ planetstack/core/xoslib/static/js/xoslib/xos-backbone.js
@@ -226,4 +226,16 @@ if (! window.XOSLIB_LOADED ) {
     };
 
     xos = new xoslib();
+
+    (function() {
+      var _sync = Backbone.sync;
+      Backbone.sync = function(method, model, options){
+        options.beforeSend = function(xhr){
+          var token = $('meta[name="csrf-token"]').attr('content');
+          xhr.setRequestHeader('X-CSRFToken', token);
+          console.log(token);
+        };
+        return _sync(method, model, options);
+      };
+    })();
 }
