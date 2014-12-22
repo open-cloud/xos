e6f4f1446faf6e9ec23ef53673173061cac4627b
Sapan Bhatia <sapanb@cs.princeton.edu>
2014-12-22 01:41:55 -0500

    BUG Fixes to sync_controller_images

--- planetstack/openstack_observer/steps/sync_controller_images.py
+++ planetstack/openstack_observer/steps/sync_controller_images.py
@@ -7,6 +7,7 @@ from observer.openstacksyncstep import OpenStackSyncStep
 from core.models import Controller
 from core.models import Image, ControllerImages
 from util.logger import Logger, logging
+from observer.ansible import *
 
 logger = Logger(level=logging.INFO)
 
@@ -23,15 +24,18 @@ class SyncControllerImages(OpenStackSyncStep):
 
     def sync_record(self, controller_image):
         logger.info("Working on image %s on controller %s" % (controller_image.image.name, controller_image.controller))
+        import pdb
+        pdb.set_trace()
 
 	image_fields = {'endpoint':controller_image.controller.auth_url,
 			'admin_user':controller_image.controller.admin_user,
 			'admin_password':controller_image.controller.admin_password,
 			'name':controller_image.image.name,
-			'filepath':controller_image.image.path
+			'filepath':controller_image.image.path,
+	                'ansible_tag': '%s@%s'%(controller_image.image.name,controller_image.controller.name), # name of ansible playbook
 			}
 
-	res = run_template('sync_controller_images.yaml', image_fields)
+	res = run_template('sync_controller_images.yaml', image_fields, path='controller_images')
 
 	if (len(res)!=1):
 	    raise Exception('Could not sync image %s'%controller_image.image.name)
