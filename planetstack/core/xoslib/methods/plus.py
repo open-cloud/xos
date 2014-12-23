a1c7b79e87949e45b89dac6cdfa76cb84c160d7d
Scott Baker <smbaker@gmail.com>
2014-12-23 11:04:27 -0800

    fix plus mixin for rest_framework 3.0

--- planetstack/core/xoslib/methods/plus.py
+++ planetstack/core/xoslib/methods/plus.py
@@ -13,6 +13,8 @@ class PlusSerializerMixin():
     # This will cause a descendant class to pull in the methods defined
     # above. See rest_framework/serializers.py: _get_declared_fields().
     base_fields = {"backendIcon": backendIcon, "backendHtml": backendHtml}
+    # Rest_framework 3.0 uses _declared_fields instead of base_fields
+    _declared_fields = {"backendIcon": backendIcon, "backendHtml": backendHtml}
 
     def getBackendIcon(self, obj):
         return obj.getBackendIcon()
