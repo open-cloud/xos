4aa7ff88737c448ef3838482fb95f14aa182d279
Scott Baker <smbaker@gmail.com>
2014-12-22 17:35:34 -0800

    rename ControllerDashboard to ControllerDashboardView

--- planetstack/core/models/dashboard.py
+++ planetstack/core/models/dashboard.py
@@ -7,15 +7,15 @@ from django.contrib.contenttypes import generic
 class DashboardView(PlCoreBase):
     name = models.CharField(max_length=200, unique=True, help_text="Name of the View")
     url = models.CharField(max_length=1024, help_text="URL of Dashboard")
-    controllers = models.ManyToManyField(Controller, blank=True, related_name="dashboards", through='ControllerDashboard')
+    controllers = models.ManyToManyField(Controller, blank=True, related_name="dashboardViews", through='ControllerDashboardView')
 
     def __unicode__(self):  return u'%s' % (self.name)
 
-class ControllerDashboard(PlCoreBase):
+class ControllerDashboardView(PlCoreBase):
     objects = ControllerLinkManager()
     deleted_objects = ControllerLinkDeletionManager()
-    controller = models.ForeignKey(Controller, related_name='controllerdashboards')
-    dashboardView = models.ForeignKey(DashboardView, related_name='controllerdashboards')
+    controller = models.ForeignKey(Controller, related_name='controllerdashboardViews')
+    dashboardView = models.ForeignKey(DashboardView, related_name='controllerdashboardViews')
 
     url = models.CharField(max_length=1024, help_text="URL of Dashboard")
 
