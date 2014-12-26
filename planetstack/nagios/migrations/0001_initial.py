a8242c7909c4e21e508bd82b0555f234c9b0bb2d
Tony Mack <tmack@tux.cs.princeton.edu>
2014-12-25 19:17:54 -0500

    adding initial migrations

---
+++ planetstack/nagios/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='NagiosService',
+            fields=[
+                ('service_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.Service')),
+            ],
+            options={
+                'verbose_name': 'Nagios Service',
+            },
+            bases=('core.service', models.Model),
+        ),
+    ]
