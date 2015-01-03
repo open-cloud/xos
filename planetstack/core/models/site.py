58cea712a343114e746c4f608aa696fbaacf2e95
Tony Mack <tmack@tux.cs.princeton.edu>
2015-01-03 17:32:23 -0500

    removing ControllerSiteDeployment object. Move tenant_id field to SiteDeployments model

--- planetstack/core/models/site.py
+++ planetstack/core/models/site.py
@@ -316,6 +316,6 @@ class SiteDeployments(PlCoreBase):
     deployment = models.ForeignKey(Deployment,related_name='sitedeployments')
     controller = models.ForeignKey(Controller, null=True, blank=True, related_name='sitedeployments')
     availability_zone = models.CharField(max_length=200, null=True, blank=True, help_text="OpenStack availability zone")
-    tenant_id = models.CharField(null=True, blank=True, max_length=200, help_text="Keystone tenant id")
+    tenant_id = models.CharField(null=True, blank=True, max_length=200, db_index=True, help_text="Keystone tenant id")
 
     def __unicode__(self):  return u'%s %s' % (self.deployment, self.site)
