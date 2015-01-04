169c723e166c5f95a5205b3719b95b664b066753
Scott Baker <smbaker@gmail.com>
2015-01-03 16:51:01 -0800

    add config file option to disable monitor

--- planetstack/monitor/__init__.py
+++ planetstack/monitor/__init__.py
@@ -1,3 +1,11 @@
-from observer import ceilometer
+from planetstack.settings import STATISTICS_DRIVER
 
-driver = ceilometer.CeilometerDriver()
+if (STATISTICS_DRIVER=="ceilometer"):
+    from observer import ceilometer
+    driver = ceilometer.CeilometerDriver()
+elif (not STATISTICS_DRIVER) or (STATISTICS_DRIVER.lower() == "none"):
+    # disabled
+    driver = None
+else:
+    driver = None
+    print "WARNING: Unknown statistics driver %s" % STATISTICS_DRIVER
