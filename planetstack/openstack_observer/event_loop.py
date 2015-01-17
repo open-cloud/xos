cb6f8d63639876f298d196990d43d8aaa5bd6e99
Sapan Bhatia <sapanb@cs.princeton.edu>
2015-01-17 01:03:52 +0000

    Add color to Observer logs

--- planetstack/openstack_observer/event_loop.py
+++ planetstack/openstack_observer/event_loop.py
@@ -22,8 +22,20 @@ from observer.steps import *
 from syncstep import SyncStep
 from toposort import toposort
 from observer.error_mapper import *
+from openstack_observer.openstacksyncstep import OpenStackSyncStep
+
 
 debug_mode = False
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 logger = Logger(level=logging.INFO)
 
