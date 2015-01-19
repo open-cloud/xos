c15ad1b8e7c6a13ecc9024c475a067b494de3e65
Scott Baker <smbaker@gmail.com>
2015-01-18 16:33:30 -0800

    add ability to disable dashboards

--- planetstack/core/models/dashboard.py
+++ planetstack/core/models/dashboard.py
@@ -8,6 +8,7 @@ class DashboardView(PlCoreBase):
     name = models.CharField(max_length=200, unique=True, help_text="Name of the View")
     url = models.CharField(max_length=1024, help_text="URL of Dashboard")
     controllers = models.ManyToManyField(Controller, blank=True, related_name="dashboardviews", through='ControllerDashboardView')
+    enabled = models.BooleanField(default=True)
 
     def __unicode__(self):  return u'%s' % (self.name)
 
@@ -16,6 +17,7 @@ class ControllerDashboardView(PlCoreBase):
     deleted_objects = ControllerLinkDeletionManager()
     controller = models.ForeignKey(Controller, related_name='controllerdashboardviews')
     dashboardView = models.ForeignKey(DashboardView, related_name='controllerdashboardviews')
+    enabled = models.BooleanField(default=True)
 
     url = models.CharField(max_length=1024, help_text="URL of Dashboard")
 
