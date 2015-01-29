8e6f53df3257067caf5716c085cbf66625fa2dbe
Sapan Bhatia <sapanb@cs.princeton.edu>
2015-01-29 21:05:39 +0000

    Django view that exports Observer status

--- planetstack/planetstack/urls.py
+++ planetstack/planetstack/urls.py
@@ -26,8 +26,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'planetstack.views.home', name='home'),
     # url(r'^planetstack/', include('planetstack.foo.urls')),
-
     url(r'^stats', 'core.views.stats.Stats', name='stats'),
+    url(r'^observer', 'core.views.observer.Observer', name='observer'),
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
