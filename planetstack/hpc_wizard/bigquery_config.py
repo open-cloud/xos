40db8e022d5922a616ca8c06653d3b59893216c8
Scott Baker <smbaker@gmail.com>
2015-02-04 16:22:05 -0800

    cleanup bigquery filenames to one spot

---
+++ planetstack/hpc_wizard/bigquery_config.py
@@ -0,0 +1,3 @@
+BIGQUERY_DIR = "/opt/planetstack"
+BIGQUERY_SECRETS_FN = os.path.join(BIGQUERY_DIR, "client_secrets.json")
+BIGQUERY_CREDENTIALS_FN = os.path.join(BIGQUERY_DIR, "bigquery_credentials.dat")
