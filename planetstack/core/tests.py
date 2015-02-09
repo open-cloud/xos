a95895dc7c63471bb0974eca9cd923b5d40c5ce4
S.Çağlar Onur <caglar@10ur.org>
2015-02-09 13:34:11 -0500

    respect the virtualenv by using /usr/bin/env
    Signed-off-by: S.Çağlar Onur <caglar@10ur.org>

--- planetstack/core/tests.py
+++ planetstack/core/tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 from django.test import TestCase
 from core.models import *
 from rest_framework.test import *
--- planetstack/syndicate_observer/syndicatelib.py
+++ planetstack/syndicate_observer/syndicatelib.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 """
 Define some common methods for the Syndicate observer.
