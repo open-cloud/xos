9edfdc04228d5d39a54e1cecf6b0b2e045d4d019
Scott Baker <smbaker@gmail.com>
2015-02-11 15:54:44 -0800

    link calendar to cloud icon

--- planetstack/core/static/observer_status.js
+++ planetstack/core/static/observer_status.js
@@ -7,10 +7,16 @@ function updateObserverStatus() {
              success: function(newData) {
                   console.log(newData);
                   if (newData.health==":-)") {
-                      html = '<span style="margin-left: 16px;" title="last observer run time = ' + Math.floor(newData.last_duration) + ' seconds"><img src="/static/img/green-cloud.gif" width=16 height=16></span>';
+                      tooltip = 'last observer run time = ' + Math.floor(newData.last_duration) + ' seconds';
+                      icon = "/static/img/green-cloud.gif";
                   } else {
-                      html = '<span style="margin-left: 16px;" title="observer is offline"><img src="/static/img/red-cloud.gif" width=16 height=16></span>';
+                      tooltip = "observer is offline";
+                      icon = "/static/img/red-cloud.gif";
                   }
+
+                  html = '<span style="margin-left: 16px; cursor: pointer;" title="' + tooltip + '"><img src="' + icon +
+                         '" width=16 height=16 onClick="showObserverCalendar();"></span>';
+
                   $("#observer-status").html(html);
                   setTimeout(function() { updateObserverStatus(); }, 60000);
              },
@@ -18,6 +24,21 @@ function updateObserverStatus() {
                   setTimeout(function() { updateObserverStatus(); }, 60000);
              }
 });
+}
+
+function showObserverCalendar() {
+    $("#dialog-placeholder").html('<iframe src="https://www.google.com/calendar/embed?src=qlnr1b3rsquq702nbns42l88s4%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>');
+    $("#dialog-placeholder").dialog({
+           autoOpen: false,
+           modal: true,
+           width: 850,
+           buttons : {
+                "Ok" : function() {
+                  $(this).dialog("close");
+                }
+              }
+            });
+        $("#dialog-placeholder").dialog("open");
 }
 
 $( document ).ready(function() {
