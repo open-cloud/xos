fe1c42d8f2ccd65ec1e047e365708d80906f9027
Scott Baker <smbaker@gmail.com>
2015-03-10 11:55:28 -0700

    fix typo

--- xos/core/models/credential.py
+++ xos/core/models/credential.py
@@ -21,7 +21,7 @@ class SiteCredential(PlCoreBase):
     site = models.ForeignKey(Site, related_name='sitecredentials', help_text="The User this credential is associated with")
 
     name = models.SlugField(help_text="The credential type, e.g. ec2", max_length=128)
-    key_id = SrippedCharField(help_text="The backend id of this credential", max_length=1024)
+    key_id = StrippedCharField(help_text="The backend id of this credential", max_length=1024)
     enc_value = EncryptedCharField(help_text="The key value of this credential", max_length=1024)
 
 
