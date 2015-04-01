a2764c04b1f2079173243150145b8e71b69b1b18
Scott Baker <smbaker@gmail.com>
2015-03-31 20:04:59 -0700

    hpc_observer support for multiple HPCs

--- xos/hpc_observer/steps/sync_originserver.py
+++ xos/hpc_observer/steps/sync_originserver.py
@@ -26,10 +26,15 @@ class SyncOriginServer(SyncStep, HpcLibrary):
         SyncStep.__init__(self, **args)
         HpcLibrary.__init__(self)
 
+    def filter_hpc_service(self, objs):
+        hpcService = self.get_hpc_service()
+
+        return [x for x in objs if x.contentProvider.serviceProvider.hpcService == hpcService]
+
     def fetch_pending(self, deleted):
         #self.consistency_check()
 
-        return SyncStep.fetch_pending(self, deleted)
+        return self.filter_hpc_service(SyncStep.fetch_pending(self, deleted))
 
     def consistency_check(self):
         # set to true if something changed
