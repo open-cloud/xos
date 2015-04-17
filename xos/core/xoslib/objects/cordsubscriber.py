808a15fb3c640473b7fa4d53a3a6a91c0915b6fb
Scott Baker <smbaker@gmail.com>
2015-04-16 21:56:03 -0700

    redo cordsubscriber fields using passthrough

--- xos/core/xoslib/objects/cordsubscriber.py
+++ xos/core/xoslib/objects/cordsubscriber.py
@@ -4,6 +4,20 @@ from plus import PlusObjectMixin
 from operator import itemgetter, attrgetter
 from rest_framework.exceptions import APIException
 
+"""
+import os
+import sys
+sys.path.append("/opt/xos")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xos.settings")
+import django
+from core.models import *
+from hpc.models import *
+from cord.models import *
+django.setup()
+from core.xoslib.objects.cordsubscriber import CordSubscriber
+c=CordSubscriber.get_tenant_objects().select_related().all()[0]
+"""
+
 class CordSubscriber(VOLTTenant, PlusObjectMixin):
     class Meta:
         proxy = True
@@ -22,29 +36,45 @@ class CordSubscriber(VOLTTenant, PlusObjectMixin):
     def vcpe_id(self, value):
         pass
 
-    @property
-    def sliver_id(self):
-        if self.vcpe and self.vcpe.sliver:
-            return self.vcpe.sliver.id
-        else:
-            return None
+    passthroughs = ( ("firewall_enable", "vcpe.firewall_enable"),
+                     ("firewall_rules", "vcpe.firewall_rules"),
+                     ("url_filter_enable", "vcpe.url_filter_enable"),
+                     ("url_filter_rules", "vcpe.url_filter_rules"),
+                     ("cdn_enable", "vcpe.cdn_enable"),
+                     ("image", "vcpe.image.id"),
+                     ("image_name", "vcpe.image.name"),
+                     ("sliver", "vcpe.sliver.id"),
+                     ("sliver_name", "vcpe.sliver.name") )
 
-    @sliver_id.setter
-    def sliver_id(self, value):
-        pass
+    def __getattr__(self, key):
+        for (member_name, passthrough_name) in self.passthroughs:
+            if key==member_name:
+                parts = passthrough_name.split(".")
+                obj = self
+                for part in parts[:-1]:
+                    obj = getattr(obj, part)
+                    if not obj:
+                        return None
+                return getattr(obj, parts[-1])
 
-    @property
-    def firewall_enable(self):
-        if self.vcpe:
-            return self.vcpe.firewall_enable
-        else:
-            return None
+        raise AttributeError("getattr: %r object has no attribute %r" %
+                         (self.__class__, key))
 
-    @firewall_enable.setter
-    def firewall_enable(self, value):
-        if self.vcpe:
-            self.vcpe.firewall_enable = value
-            # TODO: save it
+    def __setattr__(self, key, value):
+        for (member_name, passthrough_name) in self.passthroughs:
+            if key==member_name:
+                parts = passthrough_name.split(".")
+                obj = self
+                for part in parts[:-1]:
+                     obj = getattr(obj, part)
+                     if not obj:
+                         return
+                setattr(obj, parts[-1], value)
+
+        super(CordSubscriber, self).__setattr__(key, value)
+
+
+
 
 
 
