406f60b32d3b164c78fd2792d163e1c4901e2750
Scott Baker <smbaker@gmail.com>
2015-04-20 09:47:45 -0700

    sample scripts for doing REST API on vOLT tenants

---
+++ xos/cord/rest_examples/list_volt_tenants.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+HOST=198.0.0.44:8000
+AUTH=scott@onlab.us:letmein
+VLAN_ID=1234
+
+curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH $HOST/xoslib/volttenant/  
