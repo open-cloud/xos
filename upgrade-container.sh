55471311aaffc28aa2faace4dee3192edec3691d
Scott Baker <smbaker@gmail.com>
2015-05-12 11:15:10 -0700

    upgrade-container changes from demo machine

--- upgrade-container.sh
+++ upgrade-container.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 TMPDIR="/tmp/initdata"
 XOSDIR="/home/ubuntu/xos"
@@ -9,9 +9,17 @@ rm -f $TMPDIR/*.json
 XOS=$( docker ps|grep "xos:latest"|awk '{print $NF}' )
 docker exec $XOS /opt/xos/scripts/opencloud dumpdata
 docker cp $XOS:/opt/xos_backups/dumpdata-latest.json $TMPDIR
+docker cp $XOS:/opt/xos/xos_config $TMPDIR
 cp $TMPDIR/*.json $XOSDIR/xos/core/fixtures/initial_data.json
+cp $TMPDIR/xos_config $XOSDIR/xos/
 
 git pull
+
+if [[ $? != 0 ]]; then
+    echo "git pull" failed
+    exit
+fi
+
 docker build -t xos .
 
 docker stop $XOS
