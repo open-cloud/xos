3083cd4ccead4dc98b4862a3acfa2c4ab5f53bf5
Scott Baker <smbaker@gmail.com>
2015-05-21 09:56:39 -0700

    add ability to disable save buttons in admins

--- xos/xos/settings.py
+++ xos/xos/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'suit',
+    'admin_customize',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
