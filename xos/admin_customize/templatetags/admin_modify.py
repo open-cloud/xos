506d72a8ef0cf0a6f48d07fa2a0504c7ab8daa02
Scott Baker <smbaker@gmail.com>
2015-05-21 09:56:39 -0700

    add ability to disable save buttons in admins

---
+++ xos/admin_customize/templatetags/admin_modify.py
@@ -0,0 +1,12 @@
+from django.contrib.admin.templatetags.admin_modify import *
+from django.contrib.admin.templatetags.admin_modify import submit_row as original_submit_row
+
+@register.inclusion_tag('admin/submit_line.html', takes_context=True)
+def submit_row(context):
+    ctx = original_submit_row(context)
+    ctx.update({
+        'show_save': context.get('show_save', ctx['show_save']),
+        'show_save_and_add_another': context.get('show_save_and_add_another', ctx['show_save_and_add_another']),
+        'show_save_and_continue': context.get('show_save_and_continue', ctx['show_save_and_continue'])
+        })                                                                  
+    return ctx 
