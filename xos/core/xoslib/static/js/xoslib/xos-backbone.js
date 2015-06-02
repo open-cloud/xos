cf6bff9929e31dbc46a5bb93aede95c3328fdb20
Scott Baker <smbaker@gmail.com>
2015-06-01 20:35:28 -0700

    cord user administration in xoslib

--- xos/core/xoslib/static/js/xoslib/xos-backbone.js
+++ xos/core/xoslib/static/js/xoslib/xos-backbone.js
@@ -37,6 +37,7 @@ if (! window.XOSLIB_LOADED ) {
     HPCVIEW_API = XOSLIB_BASE + "/hpcview/";
 
     CORDSUBSCRIBER_API = XOSLIB_BASE + "/cordsubscriber/";
+    CORDUSER_API = XOSLIB_BASE + "/corduser/";
 
     XOSModel = Backbone.Model.extend({
         relatedCollections: [],
@@ -729,6 +730,7 @@ if (! window.XOSLIB_LOADED ) {
 
         define_model(this, {urlRoot: CORDSUBSCRIBER_API,
                             modelName: "cordSubscriber",
+                            relatedCollections: {"cordUsers": "subscriber"},
                             listFields: ["id", "vlan_id", "routeable_subnet"],
                             detailFields: ["id", "service_specific_id", "vcpe_id", "image_name", "sliver_name",
                                            "firewall_enable", "firewall_rules", "url_filter_enable", "url_filter_rules", "cdn_enable",
@@ -737,6 +739,13 @@ if (! window.XOSLIB_LOADED ) {
                             inputType: {"firewall_enable": "checkbox",
                                         "url_filter_enable": "checkbox",
                                         "cdn_enable": "checkbox"},
+                            disableAdd: true,
+                            });
+
+        define_model(this, {urlRoot: CORDUSER_API,
+                            modelName: "cordUser",
+                            listFields: ["id", "subscriber", "name", "level", "mac"],
+                            detailFields: ["subscriber", "name", "level", "mac"],
                             disableAdd: true,
                             });
 
