71d9e136dc4e0de3d16070ccac18793fa55c106f
Scott Baker <smbaker@gmail.com>
2015-07-15 14:35:57 -0700

    cleanup and checkin the example scripts

---
+++ xos/cord/rest_examples/config.sh
@@ -0,0 +1,5 @@
+#HOST=198.0.0.44:8000
+HOST=10.254.1.22:8000
+
+#AUTH=scott@onlab.us:letmein
+AUTH=padmin@vicci.org:letmein
--- xos/cord/rest_examples/delete_volt_tenant.sh
+++ xos/cord/rest_examples/delete_volt_tenant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HOST=198.0.0.44:8000
-AUTH=scott@onlab.us:letmein
-ID=974
+source ./config.sh
+
+ID=89
 
 curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X DELETE $HOST/xoslib/volttenant/$ID/  
