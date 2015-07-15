92560b5d3b7e9ab69afa9e84026ea3dfb94486bf
Scott Baker <smbaker@gmail.com>
2015-07-15 14:35:57 -0700

    cleanup and checkin the example scripts

--- xos/cord/rest_examples/add_volt_tenant.sh
+++ xos/cord/rest_examples/add_volt_tenant.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-HOST=198.0.0.44:8000
-AUTH=scott@onlab.us:letmein
-SERVICE_SPECIFIC_ID=5678
-VLAN_ID=1234
+source ./config.sh
+
+SERVICE_SPECIFIC_ID=1238
+VLAN_ID=1238
+
+echo curl "-H \"Accept: application/json; indent=4\" -H \"Content-Type: application/json\" -u $AUTH -X POST -d \"{\\\"service_specific_id\\\": \\\"$SERVICE_SPECIFIC_ID\\\", \\\"vlan_id\\\": \\\"$VLAN_ID\\\"}\" $HOST/xoslib/volttenant/"
 
 curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH -X POST -d "{\"service_specific_id\": \"$SERVICE_SPECIFIC_ID\", \"vlan_id\": \"$VLAN_ID\"}" $HOST/xoslib/volttenant/  
--- xos/cord/rest_examples/list_volt_tenants.sh
+++ xos/cord/rest_examples/list_volt_tenants.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-HOST=198.0.0.44:8000
-AUTH=scott@onlab.us:letmein
-VLAN_ID=1234
+source ./config.sh
 
 curl -H "Accept: application/json; indent=4" -H "Content-Type: application/json" -u $AUTH $HOST/xoslib/volttenant/  
