31ba9cadb1e58d3f3cc79578a4e791aac7548b2e
Scott Baker <smbaker@gmail.com>
2015-07-16 12:40:07 -0700

    allow Slice Admins to see SlicePrivileges in their slice, allow any user to be used for SlicePrivilege

--- xos/core/models/slice.py
+++ xos/core/models/slice.py
@@ -146,7 +146,18 @@ class SlicePrivilege(PlCoreBase):
         if user.is_admin:
             qs = SlicePrivilege.objects.all()
         else:
+            # You can see your own SlicePrivileges
             sp_ids = [sp.id for sp in SlicePrivilege.objects.filter(user=user)]
+
+            # A site pi or site admin can see the SlicePrivileges for all slices in his Site
+            for priv in SitePrivilege.objects.filter(user=user):
+                if priv.role.role in ['pi', 'admin']:
+                    sp_ids.extend( [sp.id for sp in SlicePrivilege.objects.filter(slice__site = priv.site)] )
+
+            # A slice admin can see the SlicePrivileges for his Slice
+            for priv in SlicePrivilege.objects.filter(user=user, role__role="admin"):
+                sp_ids.extend( [sp.id for sp in SlicePrivilege.objects.filter(slice=priv.slice)] )
+
             qs = SlicePrivilege.objects.filter(id__in=sp_ids)
         return qs
 
