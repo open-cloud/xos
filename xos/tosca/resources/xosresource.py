714d52450fc97ee3fd58afbff5b0276d869e686a
Scott Baker <smbaker@gmail.com>
2015-08-05 08:20:12 -0700

    support for scalable compute resource

--- xos/tosca/resources/xosresource.py
+++ xos/tosca/resources/xosresource.py
@@ -1,3 +1,5 @@
+import pdb
+
 class XOSResource(object):
     xos_base_class = "XOSResource"
     xos_model = None
@@ -36,6 +38,14 @@ class XOSResource(object):
         if not reqs:
             return None
         return reqs[0]
+
+    def get_scalable(self):
+        if ("capabilities" in self.nodetemplate.entity_tpl) and \
+           ("host" in self.nodetemplate.entity_tpl["capabilities"]) and \
+           ("scalable" in self.nodetemplate.entity_tpl["capabilities"]["host"]):
+               return self.nodetemplate.entity_tpl["capabilities"]["host"]["scalable"]
+        else:
+            return {}
 
     def get_xos_object(self, cls, **kwargs):
         objs = cls.objects.filter(**kwargs)
