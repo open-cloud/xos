84377381bc9fe938cba712de0762ae376cf01c61
Scott Baker <smbaker@gmail.com>
2015-08-10 17:06:42 -0700

    add hostname option to node selector

--- xos/tosca/nodeselect.py
+++ xos/tosca/nodeselect.py
@@ -4,12 +4,17 @@ import sys
 from core.models import Slice,Sliver,User,Flavor,Node,Image
 
 class XOSNodeSelector(object):
-    def __init__(self, user, mem_size=None, num_cpus=None, disk_size=None):
+    def __init__(self, user, mem_size=None, num_cpus=None, disk_size=None, hostname = None):
         self.user = user
+        self.hostname = None
 
     def get_allowed_nodes(self):
         # TODO: logic to get nodes that the user can use
         nodes = Node.objects.all()
+
+        if self.hostname:
+            nodes = nodes.filter(name = self.hostname)
+
         return nodes
 
     def get_nodes(self, quantity):
