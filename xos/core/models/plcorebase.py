3a4a5b4ce73499693124dbdf2c2ace5fbf39b654
Scott Baker <smbaker@gmail.com>
2015-08-19 16:05:25 -0700

    add no_sync field to all objects

--- xos/core/models/plcorebase.py
+++ xos/core/models/plcorebase.py
@@ -152,6 +152,7 @@ class PlCoreBase(models.Model, PlModelMixIn):
     deleted = models.BooleanField(default=False)
     write_protect = models.BooleanField(default=False)
     lazy_blocked = models.BooleanField(default=False)
+    no_sync = models.BooleanField(default=False)
 
     class Meta:
         # Changing abstract to False would require the managers of subclasses of
