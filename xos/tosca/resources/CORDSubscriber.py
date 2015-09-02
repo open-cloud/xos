26f84889953bc7f15456a9427f896afe3cf482de
Scott Baker <smbaker@gmail.com>
2015-09-01 23:45:54 -0700

    add VOLTTenant to tosca, relationships to connect VOLT to Subscriber, and update cord sample

--- xos/tosca/resources/CORDSubscriber.py
+++ xos/tosca/resources/CORDSubscriber.py
@@ -14,7 +14,7 @@ from xosresource import XOSResource
 class XOSCORDSubscriber(XOSResource):
     provides = "tosca.nodes.CORDSubscriber"
     xos_model = CordSubscriberRoot
-    copyin_props = ["subscriber_specific_id", "firewall_enable", "url_filter_enable", "cdn_enable", "url_filter_level"]
+    copyin_props = ["service_specific_id", "firewall_enable", "url_filter_enable", "cdn_enable", "url_filter_level"]
 
     def postprocess(self, obj):
         pass
