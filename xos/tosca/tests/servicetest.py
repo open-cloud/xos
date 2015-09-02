1f1f8cc9b45b9f2f2b6ac4094154e6c32cb16cfb
Scott Baker <smbaker@gmail.com>
2015-09-02 16:36:39 -0700

    more test cases

--- xos/tosca/tests/servicetest.py
+++ xos/tosca/tests/servicetest.py
@@ -4,7 +4,9 @@ from core.models import Service
 
 class ServiceTest(BaseToscaTest):
     tests = ["create_service_minimal",
-             "create_service_notpublished"]
+             "create_service_notpublished",
+             "create_service_notenabled",
+             "create_service_public_key"]
 
     def cleanup(self):
         self.try_to_delete(Service, name="test_svc")
@@ -18,6 +20,16 @@ class ServiceTest(BaseToscaTest):
         self.assert_noobj(Service, "test_svc")
         self.execute(self.make_nodetemplate("test_svc", "tosca.nodes.Service", {"published": False}))
         self.assert_obj(Service, "test_svc", kind="generic", published=False, enabled=True)
+
+    def create_service_notenabled(self):
+        self.assert_noobj(Service, "test_svc")
+        self.execute(self.make_nodetemplate("test_svc", "tosca.nodes.Service", {"enabled": False}))
+        self.assert_obj(Service, "test_svc", kind="generic", published=True, enabled=False)
+
+    def create_service_public_key(self):
+        self.assert_noobj(Service, "test_svc")
+        self.execute(self.make_nodetemplate("test_svc", "tosca.nodes.Service", {"public_key": "foobar"}))
+        self.assert_obj(Service, "test_svc", kind="generic", published=True, enabled=True, public_key="foobar")
 
 if __name__ == "__main__":
     ServiceTest()
