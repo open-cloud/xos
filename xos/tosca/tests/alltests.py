ce4a32d9ea7cc83824d7eaff9e02f17973b95047
Scott Baker <smbaker@gmail.com>
2015-09-16 16:19:11 -0700

    base test cases for Node, Site, and Deployment

--- xos/tosca/tests/alltests.py
+++ xos/tosca/tests/alltests.py
@@ -4,8 +4,14 @@ from networktest import NetworkTest
 from servicetest import ServiceTest
 from usertest import UserTest
 from computetest import ComputeTest
+from sitetest import SiteTest
+from deploymenttest import DeploymentTest
+from nodetest import NodeTest
 
 if __name__ == "__main__":
+    SiteTest()
+    DeploymentTest()
+    NodeTest()
     NetworkTest()
     PortTest()
     CoarseTenancyTest()
