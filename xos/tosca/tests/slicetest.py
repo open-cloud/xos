316103c107ec2de763ac49aaa9e1abfc78ac711d
Scott Baker <smbaker@gmail.com>
2015-09-24 15:37:55 -0700

    privilege tests for Slice and Deployment

--- xos/tosca/tests/slicetest.py
+++ xos/tosca/tests/slicetest.py
@@ -1,10 +1,11 @@
 from basetest import BaseToscaTest
 
-from core.models import Slice, Site
+from core.models import Slice, Site, User, SlicePrivilege
 
 class SliceTest(BaseToscaTest):
     tests = ["create_slice_minimal",
              "create_slice_maximal",
+             "create_slice_privilege",
              "destroy_slice"]
 
     def cleanup(self):
@@ -28,6 +29,19 @@ class SliceTest(BaseToscaTest):
                                              props={"enabled": False, "description": "foo", "slice_url": "http://foo.com/", "max_instances": 11},
                                              reqs=[("testsite", "tosca.relationships.MemberOfSite")]))
         self.assert_obj(Slice, "testsite_testslice", enabled=False, description="foo", slice_url="http://foo.com/", max_instances=11)
+
+    def create_slice_privilege(self):
+        self.assert_noobj(Slice, "testsite_testslice")
+        self.execute(self.get_base_templates() +
+                     self.make_user_template() +
+                     self.make_nodetemplate("testsite_testslice", "tosca.nodes.Slice",
+                                            reqs=[("testsite", "tosca.relationships.MemberOfSite"),
+                                                  ("test@user.com", "tosca.relationships.AdminPrivilege")]))
+        slice = self.assert_obj(Slice, "testsite_testslice")
+        user = User.objects.get(email="test@user.com")
+
+        dps = SlicePrivilege.objects.filter(user=user, slice=slice)
+        assert(len(dps) == 1)
 
     def destroy_slice(self):
         self.assert_noobj(Slice, "testsite_testslice")
