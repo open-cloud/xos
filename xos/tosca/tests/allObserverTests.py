51f3f84943e00e0f0426ce6f09ec7c58f20a1b1c
Scott Baker <smbaker@gmail.com>
2015-09-28 11:52:56 -0700

    script to run all observer tests

---
+++ xos/tosca/tests/allObserverTests.py
@@ -0,0 +1,6 @@
+from observerComputeTest import ObserverComputeTest
+from observerImageTest import ObserverImageTest
+
+if __name__ == "__main__":
+    ObserverComputeTest()
+    ObserverImageTest()
