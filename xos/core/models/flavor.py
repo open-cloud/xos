8021d0d0ed19acc17a90a0240e645ba71e536baa
Sapan Bhatia <gwsapan@gmail.com>
2015-10-07 17:31:58 +0200

    Resolve deadlock caused in a single synchronizer by cross-synchronizer dependency management mechanisms.

--- xos/core/models/flavor.py
+++ xos/core/models/flavor.py
@@ -18,6 +18,10 @@ class Flavor(PlCoreBase):
         app_label = "core"
         ordering = ('order', 'name')
 
+    def __init__(self, *args, **kwargs):
+        super(Flavor, self).__init__(*args, **kwargs)
+        self.no_sync=True
+
     def __unicode__(self):  return u'%s' % (self.name)
 
 """ FlavorParameterType and FlavorParameter are below for completeness sake,
