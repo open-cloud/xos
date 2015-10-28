2c66f964a2d3a4d1bfec9c2309d0572a33fc80e7
Scott Baker <smbaker@gmail.com>
2015-10-28 16:20:48 -0700

    push ceilometer_url file into /root/setup/

--- xos/configurations/bash/Dockerfile.bash
+++ xos/configurations/bash/Dockerfile.bash
@@ -1,8 +1,10 @@
 RUN mkdir -p /root/setup
 ADD xos/configurations/common/admin-openrc.sh /root/setup/
+ADD xos/configurations/common/controller_settings /root/setup/
 ADD xos/configurations/common/flat_net_name /root/setup/
 ADD xos/configurations/common/cloudlab-nodes.yaml /opt/xos/configurations/commmon/
 ADD xos/configurations/common/id_rsa.pub /root/setup/padmin_public_key
+ADD xos/configurations/common/ceilometer_url /root/setup/ceilometer_url
 
 CMD /usr/bin/make -C /opt/xos/configurations/bash -f Makefile.inside; /bin/bash
 
