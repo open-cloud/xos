6be0cd2e7cd9287be3569ebc0d04156ce695ba29
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-11-03 16:27:07 +0100

    Yeoman generator for ngViews is working

--- xos/core/xoslib/ngXosLib/generator-xos/app/templates/gulpfile.js
+++ xos/core/xoslib/ngXosLib/generator-xos/app/templates/gulpfile.js
@@ -8,7 +8,9 @@ var options = {
   scripts: 'src/js/',
   tmp: 'src/.tmp',
   dist: 'dist/',
-  api: '../../ngXosLib/api/'
+  api: '../../ngXosLib/api/',
+  static: '../../static/', // this is the django static folder
+  dashboards: '../../dashboards/' // this is the django html folder
 };
 
 wrench.readdirSyncRecursive('./gulp')
