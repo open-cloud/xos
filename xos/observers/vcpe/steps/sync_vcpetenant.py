89a338c5c44192a434ec8a9b7438d8a1a908ea83
Scott Baker <smbaker@gmail.com>
2015-11-09 20:25:21 -0800

    add c_tag and s_tag

--- xos/observers/vcpe/steps/sync_vcpetenant.py
+++ xos/observers/vcpe/steps/sync_vcpetenant.py
@@ -113,15 +113,21 @@ class SyncVCPETenant(SyncInstanceUsingAnsible):
             logger.info("neither bbs_slice nor bbs_server is configured in the vCPE")
 
         vlan_ids = []
+        s_tags = []
+        c_tags = []
         if o.volt:
-            vlan_ids.append(o.volt.vlan_id)
+            vlan_ids.append(o.volt.vlan_id)  # XXX remove this
+            s_tags.append(o.volt.s_tag)
+            c_tags.append(o.volt.c_tag)
 
         try:
             full_setup = Config().observer_full_setup
         except:
             full_setup = True
 
-        fields = {"vlan_ids": vlan_ids,
+        fields = {"vlan_ids": vlan_ids,   # XXX remove this
+                "s_tags": s_tags,
+                "c_tags": c_tags,
                 "dnsdemux_ip": dnsdemux_ip,
                 "cdn_prefixes": cdn_prefixes,
                 "bbs_addrs": bbs_addrs,
