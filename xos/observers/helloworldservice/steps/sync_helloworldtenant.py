3efd587c700cc1c3fe87bb9ddbc512c4262a7e1e
Jeremy Mowery <jermowery@email.arizona.edu>
2015-11-12 16:30:21 -0500

    Cleanup admin.py

--- xos/observers/helloworldservice/steps/sync_helloworldtenant.py
+++ xos/observers/helloworldservice/steps/sync_helloworldtenant.py
@@ -19,9 +19,21 @@ class SyncHelloWorldServiceTenant(SyncInstanceUsingAnsible):
     requested_interval=0
     template_name = "sync_helloworldtenant.yaml"
     service_key_name = "/opt/xos/observers/helloworldservice/helloworldservice_private_key"
+
+    def __init__(self, *args, **kwargs):
+        super(SyncHelloWorldServiceTenant, self).__init__(*args, **kwargs)
+
+    def fetch_pending(self, deleted):
+        if (not deleted):
+            objs = HelloWorldTenant.get_tenant_objects().filter(Q(enacted__lt=F('updated')) | Q(enacted=None),Q(lazy_blocked=False))
+        else:
+            objs = HelloWorldTenant.get_deleted_tenant_objects()
+
+        return objs
     
     def get_extra_attributes(self, o):
+	self.get_instance(o)
 	return {"display_message": o.display_message}
 
     def delete_record(self, m):
-        return
+        pass
