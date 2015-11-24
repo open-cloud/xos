b2fc3da95a74544a331ddeb07b4909120e667bed
smbaker <smbaker@localhost.localdomain>
2015-11-24 15:01:12 -0800

    test case for observer containers

--- xos/tosca/tests/allObserverTests.py
+++ xos/tosca/tests/allObserverTests.py
@@ -1,4 +1,5 @@
 from observerVMTest import ObserverVMTest
+from observerContainerTest import ObserverContainerTest
 from observerImageTest import ObserverImageTest
 from observerUserTest import ObserverUserTest
 from observerSiteTest import ObserverSiteTest
@@ -6,6 +7,7 @@ from observerSliceTest import ObserverSliceTest
 
 if __name__ == "__main__":
     ObserverVMTest()
+    ObserverContainerTest()
     ObserverImageTest()
     ObserverSiteTest()
     ObserverUserTest()
