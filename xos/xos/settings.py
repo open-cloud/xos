555bad533b4c55134fc0e5b9e3e6b81501649390
Matteo Scandolo <teo@onlab.us>
2015-12-01 17:10:00 -0800

    Added suite

--- xos/xos/settings.py
+++ xos/xos/settings.py
@@ -143,7 +143,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    #'suit',
+    'suit',
     'xos.apps.MyDjangoSuitConfig',
     'admin_customize',
     'django.contrib.admin',
