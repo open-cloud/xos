eb1e53a6a2fb3dca016d54c4513bb6d7fd7d5aa3
Matteo Scandolo <teo@onlab.us>
2015-12-16 16:23:18 -0800

    Formatting data to a flat list

--- xos-apps/auto-scale/gui/src/js/main.js
+++ xos-apps/auto-scale/gui/src/js/main.js
@@ -6,10 +6,6 @@ angular.module('autoscaling', [
   'ngAnimate',
   'chart.js'
 ])
-.run((Autoscaling) => {
-  // start polling data
-  Autoscaling.getAutoscalingData();
-})
 .config(($stateProvider, $urlRouterProvider) => {
   $stateProvider
   .state('ceilometerDashboard', {
