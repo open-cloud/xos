a4f786147db55b4ef4349900bbe7ec9b7746efc5
Jeremy Mowery <jermowery@email.arizona.edu>
2016-01-04 14:53:22 -0700

    autope8 and pyflakes applied to services/helloworld/models.py

--- xos/services/helloworld/models.py
+++ xos/services/helloworld/models.py
@@ -1,18 +1,15 @@
+from core.models import Instance, PlCoreBase
 from django.db import models
-from core.models import User, Service, SingletonModel, PlCoreBase, Instance
-from core.models.plcorebase import StrippedCharField
-import os
-from django.db import models
-from django.forms.models import model_to_dict
-from django.db.models import Q
 
 
 # Create your models here.
 
 class Hello(PlCoreBase):
-    name = models.CharField(max_length=254,help_text="Salutation e.g. Hello or Bonjour")
-    instance_backref = models.ForeignKey(Instance,related_name="hellos")
-    
+    name = models.CharField(
+        max_length=254, help_text="Salutation e.g. Hello or Bonjour")
+    instance_backref = models.ForeignKey(Instance, related_name="hellos")
+
+
 class World(PlCoreBase):
-    name = models.CharField(max_length=254,help_text="Name of planet")
-    hello = models.ForeignKey(Hello) 
+    name = models.CharField(max_length=254, help_text="Name of planet")
+    hello = models.ForeignKey(Hello)
