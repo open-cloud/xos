af61b53f6eed92d54aa836ee29bb993391090f4a
Jeremy Mowery <jermowery@email.arizona.edu>
2016-01-04 15:29:14 -0700

    Fix onos

--- xos/services/onos/models.py
+++ xos/services/onos/models.py
@@ -1,17 +1,9 @@
-from django.db import models
-from core.models import Service, PlCoreBase, Slice, Instance, Tenant, TenantWithContainer, Node, Image, User, Flavor, Subscriber
-from core.models.plcorebase import StrippedCharField
-import os
-from django.db import models, transaction
-from django.forms.models import model_to_dict
-from django.db.models import Q
-from operator import itemgetter, attrgetter, methodcaller
-import traceback
-from xos.exceptions import *
-from core.models import SlicePrivilege, SitePrivilege
-from sets import Set
+from core.models import Service, Tenant
+from django.db import transaction
+from xos.exceptions import XOSProgrammingError
 
 ONOS_KIND = "onos"
+
 
 class ONOSService(Service):
     KIND = ONOS_KIND
@@ -31,7 +23,9 @@ class ONOSService(Service):
     def use_external_host(self, value):
         self.set_attribute("use_external_host", value)
 
+
 class ONOSApp(Tenant):   # aka 'ONOSTenant'
+
     class Meta:
         proxy = True
 
@@ -39,10 +33,12 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
 
     default_attributes = {"name": "",
                           "dependencies": ""}
+
     def __init__(self, *args, **kwargs):
         onos_services = ONOSService.get_service_objects().all()
         if onos_services:
-            self._meta.get_field("provider_service").default = onos_services[0].id
+            self._meta.get_field(
+                "provider_service").default = onos_services[0].id
         super(ONOSApp, self).__init__(*args, **kwargs)
 
     @property
@@ -50,13 +46,13 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
         from core.models import User
         if getattr(self, "cached_creator", None):
             return self.cached_creator
-        creator_id=self.get_attribute("creator_id")
+        creator_id = self.get_attribute("creator_id")
         if not creator_id:
             return None
-        users=User.objects.filter(id=creator_id)
+        users = User.objects.filter(id=creator_id)
         if not users:
             return None
-        user=users[0]
+        user = users[0]
         self.cached_creator = users[0]
         return user
 
@@ -65,7 +61,7 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
         if value:
             value = value.id
         if (value != self.get_attribute("creator_id", None)):
-            self.cached_creator=None
+            self.cached_creator = None
         self.set_attribute("creator_id", value)
 
     @property
@@ -85,7 +81,7 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
         self.set_attribute("dependencies", value)
 
     #@property
-    #def instance(self):
+    # def instance(self):
     #    instance_id = self.get_attribute("instance_id", self.default_attributes["instance_id"])
     #    if instance_id:
     #        instances = Instance.objects.filter(id=instance_id)
@@ -94,13 +90,14 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
     #    return None
 
     #@instance.setter
-    #def instance(self, value):
+    # def instance(self, value):
     #    self.set_attribute("instance_id", value.id)
 
     def save(self, *args, **kwargs):
         if not self.creator:
             if not getattr(self, "caller", None):
-                # caller must be set when creating a vCPE since it creates a slice
+                # caller must be set when creating a vCPE since it creates a
+                # slice
                 raise XOSProgrammingError("ONOSApp's self.caller was not set")
             self.creator = self.caller
             if not self.creator:
@@ -110,6 +107,8 @@ class ONOSApp(Tenant):   # aka 'ONOSTenant'
         model_policy_onos_app(self.pk)
 
 # TODO: Probably don't need this...
+
+
 def model_policy_onos_app(pk):
     # TODO: this should be made in to a real model_policy
     with transaction.atomic():
@@ -117,6 +116,4 @@ def model_policy_onos_app(pk):
         if not oa:
             return
         oa = oa[0]
-        #oa.manage_container()
-
-
+        # oa.manage_container()
