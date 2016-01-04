fbaef1882116ea2a3b27647c4fe77cc771aa354c
Jeremy Mowery <jermowery@email.arizona.edu>
2016-01-04 15:31:46 -0700

    Fix requestrouter

--- xos/services/requestrouter/migrations/0001_initial.py
+++ xos/services/requestrouter/migrations/0001_initial.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
 import django.utils.timezone
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -15,12 +15,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='RequestRouterService',
             fields=[
-                ('service_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.Service')),
-                ('behindNat', models.BooleanField(default=False, help_text=b"Enables 'Behind NAT' mode.")),
-                ('defaultTTL', models.PositiveIntegerField(default=30, help_text=b'DNS response time-to-live(TTL)')),
-                ('defaultAction', models.CharField(default=b'best', help_text=b'Review if this should be enum', max_length=30)),
-                ('lastResortAction', models.CharField(default=b'random', help_text=b'Review if this should be enum', max_length=30)),
-                ('maxAnswers', models.PositiveIntegerField(default=3, help_text=b'Maximum number of answers in DNS response.')),
+                ('service_ptr', models.OneToOneField(parent_link=True, auto_created=True,
+                                                     primary_key=True, serialize=False, to='core.Service')),
+                ('behindNat', models.BooleanField(default=False,
+                                                  help_text=b"Enables 'Behind NAT' mode.")),
+                ('defaultTTL', models.PositiveIntegerField(
+                    default=30, help_text=b'DNS response time-to-live(TTL)')),
+                ('defaultAction', models.CharField(default=b'best',
+                                                   help_text=b'Review if this should be enum', max_length=30)),
+                ('lastResortAction', models.CharField(default=b'random',
+                                                      help_text=b'Review if this should be enum', max_length=30)),
+                ('maxAnswers', models.PositiveIntegerField(default=3,
+                                                           help_text=b'Maximum number of answers in DNS response.')),
             ],
             options={
                 'verbose_name': 'Request Router Service',
@@ -30,18 +36,29 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ServiceMap',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('created', models.DateTimeField(default=django.utils.timezone.now, auto_now_add=True)),
-                ('updated', models.DateTimeField(default=django.utils.timezone.now, auto_now=True)),
-                ('enacted', models.DateTimeField(default=None, null=True, blank=True)),
-                ('backend_status', models.CharField(default=b'Provisioning in progress', max_length=140)),
+                ('id', models.AutoField(verbose_name='ID',
+                                        serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(
+                    default=django.utils.timezone.now, auto_now_add=True)),
+                ('updated', models.DateTimeField(
+                    default=django.utils.timezone.now, auto_now=True)),
+                ('enacted', models.DateTimeField(
+                    default=None, null=True, blank=True)),
+                ('backend_status', models.CharField(
+                    default=b'Provisioning in progress', max_length=140)),
                 ('deleted', models.BooleanField(default=False)),
-                ('name', models.SlugField(help_text=b'name of this service map', unique=True)),
-                ('prefix', models.CharField(help_text=b'FQDN of the region of URI space managed by RR on behalf of this service', max_length=256)),
-                ('siteMap', models.FileField(help_text=b'maps client requests to service instances', upload_to=b'maps/', blank=True)),
-                ('accessMap', models.FileField(help_text=b'specifies which client requests are allowed', upload_to=b'maps/', blank=True)),
-                ('owner', models.ForeignKey(help_text=b'service which owns this map', to='core.Service')),
-                ('slice', models.ForeignKey(help_text=b'slice that implements this service', to='core.Slice')),
+                ('name', models.SlugField(
+                    help_text=b'name of this service map', unique=True)),
+                ('prefix', models.CharField(
+                    help_text=b'FQDN of the region of URI space managed by RR on behalf of this service', max_length=256)),
+                ('siteMap', models.FileField(
+                    help_text=b'maps client requests to service instances', upload_to=b'maps/', blank=True)),
+                ('accessMap', models.FileField(
+                    help_text=b'specifies which client requests are allowed', upload_to=b'maps/', blank=True)),
+                ('owner', models.ForeignKey(
+                    help_text=b'service which owns this map', to='core.Service')),
+                ('slice', models.ForeignKey(
+                    help_text=b'slice that implements this service', to='core.Slice')),
             ],
             options={
             },
--- xos/services/requestrouter/models.py
+++ xos/services/requestrouter/models.py
@@ -1,35 +1,48 @@
-from core.models import User,Site,Service,SingletonModel,PlCoreBase, Slice
-import os
+
+from core.models import PlCoreBase, Service, SingletonModel, Slice
 from django.db import models
-from django.forms.models import model_to_dict
+
 
 # Create your models here.
 
-class RequestRouterService(SingletonModel,Service):
+class RequestRouterService(SingletonModel, Service):
+
     class Meta:
         app_label = "requestrouter"
         verbose_name = "Request Router Service"
 
-    behindNat = models.BooleanField(default=False, help_text="Enables 'Behind NAT' mode.")
-    defaultTTL = models.PositiveIntegerField(default=30, help_text="DNS response time-to-live(TTL)")
-    defaultAction = models.CharField(max_length=30, default = "best", help_text="Review if this should be enum")
-    lastResortAction = models.CharField(max_length=30, default = "random", help_text="Review if this should be enum")
-    maxAnswers = models.PositiveIntegerField(default=3, help_text="Maximum number of answers in DNS response.")
+    behindNat = models.BooleanField(
+        default=False, help_text="Enables 'Behind NAT' mode.")
+    defaultTTL = models.PositiveIntegerField(
+        default=30, help_text="DNS response time-to-live(TTL)")
+    defaultAction = models.CharField(
+        max_length=30, default="best", help_text="Review if this should be enum")
+    lastResortAction = models.CharField(
+        max_length=30, default="random", help_text="Review if this should be enum")
+    maxAnswers = models.PositiveIntegerField(
+        default=3, help_text="Maximum number of answers in DNS response.")
 
-    def __unicode__(self):  return u'Request Router Service'
+    def __unicode__(self): return u'Request Router Service'
+
 
 class ServiceMap(PlCoreBase):
 
     class Meta:
         app_label = "requestrouter"
 
-    name = models.SlugField(max_length=50, unique=True, blank=False, null=False, help_text="name of this service map")
+    name = models.SlugField(max_length=50, unique=True, blank=False,
+                            null=False, help_text="name of this service map")
     owner = models.ForeignKey(Service, help_text="service which owns this map")
-    slice = models.ForeignKey(Slice, help_text="slice that implements this service")
-    prefix = models.CharField(max_length=256, help_text="FQDN of the region of URI space managed by RR on behalf of this service")
-	# need to fix the upload location appropriately, for now we are not using access/site map
-    siteMap = models.FileField(upload_to="maps/", help_text="maps client requests to service instances", blank=True)
-    accessMap = models.FileField(upload_to="maps/", help_text="specifies which client requests are allowed", blank=True)
+    slice = models.ForeignKey(
+        Slice, help_text="slice that implements this service")
+    prefix = models.CharField(
+        max_length=256, help_text="FQDN of the region of URI space managed by RR on behalf of this service")
+    # need to fix the upload location appropriately, for now we are not using
+    # access/site map
+    siteMap = models.FileField(
+        upload_to="maps/", help_text="maps client requests to service instances", blank=True)
+    accessMap = models.FileField(
+        upload_to="maps/", help_text="specifies which client requests are allowed", blank=True)
 
     def siteMapName(self):
         return self.name + ".site"
@@ -38,4 +51,3 @@ class ServiceMap(PlCoreBase):
         return self.name + ".access"
 
     def __unicode__(self): return u'%s' % self.name
-
