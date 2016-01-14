171c23b9217a57d13818089308ea5d277656e580
Scott Baker <smbaker@gmail.com>
2016-01-14 09:27:28 -0800

    rename scripts/opencloud to tools/xos-manage

--- xos/manage.py
+++ xos/manage.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     if "--makemigrations" in sys.argv:
-        os.system("/opt/xos/scripts/opencloud makemigrations")
+        os.system("/opt/xos/tools/xos-manage makemigrations")
         sys.argv.remove("--makemigrations")
 
     if "--nomodelpolicy" in sys.argv:
