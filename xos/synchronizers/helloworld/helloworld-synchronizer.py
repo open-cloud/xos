c1a07f9407c5e7fac2079cb16d1c91564a651f3f
Sapan Bhatia <gwsapan@gmail.com>
2016-01-18 10:50:21 -0500

    Updated service synchronizers with the right path

--- xos/synchronizers/helloworld/helloworld-synchronizer.py
+++ xos/synchronizers/helloworld/helloworld-synchronizer.py
@@ -5,7 +5,7 @@
 import importlib
 import os
 import sys
-observer_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"../..")
+observer_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../synchronizers/base")
 sys.path.append(observer_path)
-mod = importlib.import_module("xos-observer")
+mod = importlib.import_module("xos-synchronizer")
 mod.main()
