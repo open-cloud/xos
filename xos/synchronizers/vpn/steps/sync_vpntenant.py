2a9d50501d09ee2b891cccf99d4bbd6e3679c70c
Jeremy Mowery <jermowery@email.arizona.edu>
2016-01-24 19:45:13 -0700

    Refactor to match syncrhonizer changes

--- xos/synchronizers/vpn/steps/sync_vpntenant.py
+++ xos/synchronizers/vpn/steps/sync_vpntenant.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from django.db.models import Q, F
-from observers.base.SyncInstanceUsingAnsible import SyncInstanceUsingAnsible
+from synchronizers.base.SyncInstanceUsingAnsible import SyncInstanceUsingAnsible
 from services.vpn.models import VPNTenant
 
 parentdir = os.path.join(os.path.dirname(__file__), "..")
