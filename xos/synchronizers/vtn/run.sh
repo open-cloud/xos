14160f264a791dfdbb7cac95bf19c18631a163ed
Scott Baker <smbaker@gmail.com>
2016-02-02 10:18:36 -0800

    fix config file names

--- xos/synchronizers/vtn/run.sh
+++ xos/synchronizers/vtn/run.sh
@@ -1,2 +1,2 @@
 export XOS_DIR=/opt/xos
-python vtn-synchronizer.py  -C $XOS_DIR/observers/vtn/vtn_synchronizer_config
+python vtn-synchronizer.py  -C $XOS_DIR/synchronizers/vtn/vtn_synchronizer_config
