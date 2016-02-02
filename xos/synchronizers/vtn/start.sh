071c7f568ef55f2fcf37ba336a1ea5cefb490e64
Scott Baker <smbaker@gmail.com>
2016-02-02 10:15:18 -0800

    fix file names: vtn-observer.py -> vtn-synchronizer.py

--- xos/synchronizers/vtn/start.sh
+++ xos/synchronizers/vtn/start.sh
@@ -1,2 +1,2 @@
 export XOS_DIR=/opt/xos
-nohup python vtn-observer.py  -C $XOS_DIR/observers/vtn/vtn_observer_config > /dev/null 2>&1 &
+nohup python vtn-synchronizer.py  -C $XOS_DIR/observers/vtn/vtn_observer_config > /dev/null 2>&1 &
