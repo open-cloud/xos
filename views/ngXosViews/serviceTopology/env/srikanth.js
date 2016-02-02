d15d3bbd46cce77e3b5ca67bbadcf7106a7b66d3
Matteo Scandolo <teo@onlab.us>
2016-02-02 15:55:00 -0800

    Updated ENV

--- views/ngXosViews/serviceTopology/env/srikanth.js
+++ views/ngXosViews/serviceTopology/env/srikanth.js
@@ -8,6 +8,6 @@
 
 module.exports = {
   host: 'http://130.127.133.41:9999',
-  xoscsrftoken: 'RfURkVJbCMk6xQPPlNrtPwfD3PXADExp',
-  xossessionid: 'dkar580x0b0z14vz9j198s1ns0m6g3cb'
+  xoscsrftoken: 'yxEhnmrR0GrRaAbTz3tiCEh1DzFDQ98v',
+  xossessionid: 'hgf01hgpsrw7g4mveuajuo6p7hezm1c5'
 };
