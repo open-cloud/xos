42b2c01c2d20a58f54c7a99c64744c9147c303de
Jeremy Mowery <jermowery@email.arizona.edu>
2016-02-25 11:25:53 -0700

    Set up first page of VPN Dashboard attempt 1

--- xos/core/xoslib/methods/vpnview.py
+++ xos/core/xoslib/methods/vpnview.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from services.vpn.models import VPNTenant
 
 class VpnTenantsList(APIView):
     method_kind = "list"
     method_name = "vpntenants"
 
     def get(self, request, format=None):
-        return Response(list())
+        return Response(VPNTenant.get_tenant_objects())
