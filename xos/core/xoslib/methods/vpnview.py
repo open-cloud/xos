4f1560c0b9bb2584236884bf782081049299479b
Jeremy Mowery <jermowery@email.arizona.edu>
2016-02-25 16:16:23 -0700

    Start to create method to download script

--- xos/core/xoslib/methods/vpnview.py
+++ xos/core/xoslib/methods/vpnview.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from services.vpn.models import VPNTenant
@@ -7,4 +8,15 @@ class VpnTenantsList(APIView):
     method_name = "vpntenants"
 
     def get(self, request, format=None):
+        if (not request.user.is_authenticated()):
+            raise PermissionDenied("You must be authenticated in order to use this API")
+        return Response(VPNTenant.get_tenant_objects())
+
+class ClientScript(APIView):
+    method_kind = "detail"
+    method_name = "clientscript"
+
+    def get(self, request, format=None):
+        if (not request.user.is_authenticated()):
+            raise PermissionDenied("You must be authenticated in order to use this API")
         return Response(VPNTenant.get_tenant_objects())
