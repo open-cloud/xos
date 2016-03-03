735e075e01fe29d98ee3da7b995934519c4da9f7
Scott Baker <smbaker@gmail.com>
2016-03-02 17:06:15 -0800

    recipes for setting up cmi

---
+++ xos/configurations/cord-pod/cdn/cmi-settings.sh
@@ -0,0 +1,7 @@
+# This holds the connection information necessary to talk to your CMI.
+# It will be used by setup-cmi.sh and ssh-cmi.sh
+
+COMPUTE_NODE=cp-2.smbaker-xos-vtn.xos-pg0.clemson.cloudlab.us
+MGMT_IP=172.27.0.17
+NODE_KEY=/root/setup/id_rsa.sh
+VM_KEY=cmi_id_rsa
---
+++ xos/configurations/cord-pod/cdn/templates/setup_cmi_node.sh
@@ -0,0 +1,12 @@
+site_id=GetSites()[0]["site_id"]
+nodeinfo = {'hostname': "{{ node_hostname }}", 'dns': "8.8.8.8"}
+n_id = AddNode(site_id, nodeinfo)
+mac = "DE:AD:BE:EF:00:01"
+interfacetemplate = {'mac': mac, 'kind': 'physical', 'method': 'static', 'is_primary': True, 'if_name': 'eth0'}
+i_id = AddInterface(n_id, interfacetemplate)
+ip_addr = "169.254.169.1" # TO DO: get this from Neutron in the future
+netmask = "255.255.255.254" # TO DO: get this from Neutron in the future
+ipinfo = {'ip_addr': ip_addr, 'netmask': netmask, 'type': 'ipv4'}
+ip_id = AddIpAddress(i_id, ipinfo)
+routeinfo = {'interface_id': i_id, 'next_hop': "127.0.0.127", 'subnet': '0.0.0.0', 'metric': 1}
+r_id = AddRoute(n_id, routeinfo)
