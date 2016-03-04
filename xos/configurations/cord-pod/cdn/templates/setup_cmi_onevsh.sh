64e9b31491e58b0e1fa6220cf70265296a3c66c4
Scott Baker <smbaker@gmail.com>
2016-03-03 22:26:19 -0800

    ListAll modified the dictionary; change the Origin URL slightly

--- xos/configurations/cord-pod/cdn/templates/setup_cmi_onevsh.sh
+++ xos/configurations/cord-pod/cdn/templates/setup_cmi_onevsh.sh
@@ -1,5 +1,5 @@
 def CreateOrFind(kind, args):
-    objs=ListAll(kind, args)
+    objs=ListAll(kind, args.copy())
     if objs:
         id_name = {"ServiceProvider": "service_provider_id",
                    "ContentProvider": "content_provider_id",
@@ -12,5 +12,5 @@ def CreateOrFind(kind, args):
         return Create(kind, args)
 sp=CreateOrFind("ServiceProvider", {"account": "cord", "name": "cord", "enabled": True})
 cp=CreateOrFind("ContentProvider", {"account": "test", "name": "test", "enabled": True, "service_provider_id": sp})
-ors=CreateOrFind("OriginServer", {"url": "http://www.cs.arizona.edu/", "content_provider_id": cp, "service_type": "HyperCache"})
-pre=CreateOrFind("CDNPrefix", {"service": "HyperCache", "enabled": True, "content_provider_id": cp, "cdn_prefix": "test.vicci.org", "default_origin_server": "http://www.cs.arizona.edu/"})
+ors=CreateOrFind("OriginServer", {"url": "http://www.cs.arizona.edu", "content_provider_id": cp, "service_type": "HyperCache"})
+pre=CreateOrFind("CDNPrefix", {"service": "HyperCache", "enabled": True, "content_provider_id": cp, "cdn_prefix": "test.vicci.org", "default_origin_server": "http://www.cs.arizona.edu"})
