e2e993817ac08404f6dc1b59d9d6ed62e9960391
Scott Baker <smbaker@gmail.com>
2016-03-03 20:53:56 -0800

    run cmi setup scripts, better idempotency

--- xos/configurations/cord-pod/cdn/cmi-settings.sh
+++ xos/configurations/cord-pod/cdn/cmi-settings.sh
@@ -2,6 +2,6 @@
 # It will be used by setup-cmi.sh and ssh-cmi.sh
 
 COMPUTE_NODE=cp-2.smbaker-xos-vtn.xos-pg0.clemson.cloudlab.us
-MGMT_IP=172.27.0.17
-NODE_KEY=/root/setup/id_rsa.sh
+MGMT_IP=172.27.0.22
+NODE_KEY=/root/setup/id_rsa
 VM_KEY=cmi_id_rsa
