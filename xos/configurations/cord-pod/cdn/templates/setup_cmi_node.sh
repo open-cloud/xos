7971dfd5d5379b51aee2d13ced8ad81606d8cc93
Scott Baker <smbaker@gmail.com>
2016-03-03 20:53:56 -0800

    run cmi setup scripts, better idempotency

--- xos/configurations/cord-pod/cdn/templates/setup_cmi_node.sh
+++ xos/configurations/cord-pod/cdn/templates/setup_cmi_node.sh
@@ -10,3 +10,11 @@ ipinfo = {'ip_addr': ip_addr, 'netmask': netmask, 'type': 'ipv4'}
 ip_id = AddIpAddress(i_id, ipinfo)
 routeinfo = {'interface_id': i_id, 'next_hop': "127.0.0.127", 'subnet': '0.0.0.0', 'metric': 1}
 r_id = AddRoute(n_id, routeinfo)
+hpc_slice_id = GetSlices({"name": "co_coblitz"})[0]["slice_id"]
+AddSliceToNodes(hpc_slice_id, [n_id])
+dnsdemux_slice_id = GetSlices({"name": "co_dnsdemux"})[0]["slice_id"]
+dnsredir_slice_id = GetSlices({"name": "co_dnsredir_coblitz"})[0]["slice_id"]
+AddSliceToNodes(dnsdemux_slice_id, [n_id])
+AddSliceToNodes(dnsredir_slice_id, [n_id])
+takeoverscript=GetBootMedium(n_id, "node-cloudinit", '')
+file("/root/takeover-{{ node_hostname }}","w").write(takeoverscript)
