99fb3e78599ce4f183c6caee7580e041cb8e9c4a
Sapan Bhatia <gwsapan@gmail.com>
2016-03-24 07:56:38 +0100

    Synchronizer diagnostics

--- xos/core/views/observer.py
+++ xos/core/views/observer.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
 from core.models import *
+from xos.config import Config
 import json
 import os
 import time
 
 def Observer(request):
-    if not os.path.exists('/tmp/observer_last_run'):
+    diag = Diag.objects.filter(name=Config().observer_name).first()
+    if not diag:
         return HttpResponse(json.dumps({"health": ":-X", "time": time.time(), "comp": 0}))
 
     t = time.time()
-    status_str = open('/tmp/observer_last_run','r').read()
-    d = json.loads(status_str)
+    
+    d = json.loads(diag.backend_register)
     comp = d['last_run'] + d['last_duration']*2 + 300
     if comp>t:
         d['health'] = ':-)'
