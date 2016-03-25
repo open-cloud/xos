86e446d335ed093d8e6b9028a3ae2fedbd8e13fa
Matteo Scandolo <teo@onlab.us>
2016-03-25 14:16:51 -0700

    Reading only user dashboards

--- xos/core/templatetags/core_tags.py
+++ xos/core/templatetags/core_tags.py
@@ -1,13 +1,16 @@
 from django import template
 import sys
 from core.models import DashboardView
+from itertools import chain
 
 register = template.Library()
 
-print >>sys.stderr, 'Got loaded!'
-@register.inclusion_tag('admin/tags/dashboard_list.html')
-def dashboard_list():
-    dashboards = DashboardView.objects.all()
-    print >>sys.stderr, 'Requesting template!'
-    print >>sys.stderr, dashboards
-    return {'dashboards': dashboards}
+@register.inclusion_tag('admin/tags/dashboard_list.html', takes_context=True)
+def dashboard_list(context):
+    request = context['request']
+    dashboards = request.user.get_dashboards()
+    customize = DashboardView.objects.filter(name="Customize")
+    # debug = DashboardView.objects.filter(name="Customize").values()
+    print >>sys.stderr, request.user.get_dashboards()
+    result_list = list(chain(dashboards, customize))
+    return {'dashboards': result_list}
