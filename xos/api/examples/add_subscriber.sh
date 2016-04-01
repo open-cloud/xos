69c5688bc611c04c62f277e12da491447a373cb2
Scott Baker <smbaker@gmail.com>
2016-04-01 09:17:35 -0700

    support subscriber name field

--- xos/api/examples/add_subscriber.sh
+++ xos/api/examples/add_subscriber.sh
@@ -5,7 +5,7 @@ source ./config.sh
 ACCOUNT_NUM=1238
 
 DATA=$(cat <<EOF
-{"identity": {"account_num": "$ACCOUNT_NUM"},
+{"identity": {"account_num": "$ACCOUNT_NUM", "name": "test-subscriber"},
  "features": {"uplink_speed": 2000000000}}
 EOF
 )
