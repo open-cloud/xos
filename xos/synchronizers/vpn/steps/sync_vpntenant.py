b3c735be244ffd06886bdad100d3a417568f6207
Jeremy Mowery <jermowery@email.arizona.edu>
2016-04-02 10:16:21 -0700

    Move to better way of handling multiple pkis

--- xos/synchronizers/vpn/steps/sync_vpntenant.py
+++ xos/synchronizers/vpn/steps/sync_vpntenant.py
@@ -2,11 +2,9 @@ import os
 import sys
 
 from django.db.models import F, Q
-from services.vpn.models import VPNTenant
-from subprocess import Popen, PIPE
+from services.vpn.models import VPNService, VPNTenant
 from synchronizers.base.SyncInstanceUsingAnsible import \
     SyncInstanceUsingAnsible
-from xos.exceptions import XOSConfigurationError
 
 parentdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, parentdir)
@@ -26,7 +24,8 @@ class SyncVPNTenant(SyncInstanceUsingAnsible):
     def fetch_pending(self, deleted):
         if (not deleted):
             objs = VPNTenant.get_tenant_objects().filter(
-                Q(enacted__lt=F('updated')) | Q(enacted=None), Q(lazy_blocked=False))
+                Q(enacted__lt=F('updated')) |
+                Q(enacted=None), Q(lazy_blocked=False))
         else:
             objs = VPNTenant.get_deleted_tenant_objects()
 
@@ -36,18 +35,19 @@ class SyncVPNTenant(SyncInstanceUsingAnsible):
         return {"is_persistent": tenant.is_persistent,
                 "vpn_subnet": tenant.vpn_subnet,
                 "server_network": tenant.server_network,
-                "clients_can_see_each_other": tenant.clients_can_see_each_other,
+                "clients_can_see_each_other": (
+                    tenant.clients_can_see_each_other),
                 "tenant_id": tenant.id,
                 "port_number": tenant.port_number,
-                "protocol": tenant.protocol
+                "protocol": tenant.protocol,
+                "pki_dir": (
+                    VPNService.OPENVPN_PREFIX + "server-" + str(tenant.id))
                 }
 
     def run_playbook(self, o, fields):
         # Generate the server files
-        (stdout, stderr) = Popen("/opt/openvpn/easyrsa3/server-" + o.id + "/easyrsa --batch build-server-full server nopass", shell=True, stdout=PIPE, stderr=PIPE).communicate()
-        if (stderr):
-            raise XOSConfigurationError("build-server-full failed with standard out:" + str(stdout) + " and stderr: " + str(stderr))
-        (stdout, stderr) = Popen("/opt/openvpn/easyrsa3/server-" + o.id + "/easyrsa --batch gen-crl", shell=True, stdout=PIPE, stderr=PIPE).communicate()
-        if (stderr):
-            raise XOSConfigurationError("gen-crl failed with standard out:" + str(stdout) + " and stderr: " + str(stderr))
+        pki_dir = VPNService.OPENVPN_PREFIX + "server-" + str(o.id)
+        VPNService.execute_easyrsa_command(
+            pki_dir, "build-server-full server nopass")
+        VPNService.execute_easyrsa_command(pki_dir, "gen-crl")
         super(SyncVPNTenant, self).run_playbook(o, fields)
