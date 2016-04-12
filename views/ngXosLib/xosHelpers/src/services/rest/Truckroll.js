c7987cd6ab3b59860e0244d503948f49b014ea4b
Matteo Scandolo <teo@onlab.us>
2016-04-12 11:38:40 -0700

    Implemented new API generator

---
+++ views/ngXosLib/xosHelpers/src/services/rest/Truckroll.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+ * Virtual Truckroll, enable to perform basic test on user connectivity such as ping, traceroute and tcpdump.
+
+ */
+
+angular.module('xos.helpers')
+.service('Truckroll-Collection', function($resource){
+  return $resource('/api/tenant/truckroll/:truckroll_id/', { truckroll_id: '@id'});
+})
