57404dc72056ffe3c4743851829af192ccfff354
Matteo Scandolo <teo@onlab.us>
2016-04-13 16:20:13 -0700

    Generating api docs and added ngAnimate

--- views/ngXosLib/gulp/ngXosHelpers.js
+++ views/ngXosLib/gulp/ngXosHelpers.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var concat = require("gulp-concat");
+var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var angularFilesort = require('gulp-angular-filesort');
+var gulpDocs = require('gulp-ngdocs');
 
 module.exports = function(options){
   gulp.task('helpers', function(){
@@ -12,5 +13,17 @@ module.exports = function(options){
       .pipe(ngAnnotate())
       .pipe(uglify())
       .pipe(gulp.dest(options.ngXosVendor));
+  });
+
+  gulp.task('docs', function(){
+    return gulp.src(options.xosHelperSource + '**/*.js')
+      .pipe(gulpDocs.process({
+        title: 'XOS Helpers Module',
+        scripts: [
+          'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js',
+          'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js'
+        ]
+      }))
+      .pipe(gulp.dest('./docs'));
   });
 };
