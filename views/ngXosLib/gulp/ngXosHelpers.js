582cc81d9d193afce6c86d5751d76cda5b5e55b1
Matteo Scandolo <teo@onlab.us>
2016-04-13 17:23:28 -0700

    Added ngDoc to generated API

--- views/ngXosLib/gulp/ngXosHelpers.js
+++ views/ngXosLib/gulp/ngXosHelpers.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var angularFilesort = require('gulp-angular-filesort');
 var gulpDocs = require('gulp-ngdocs');
+var del = require('del');
 
 module.exports = function(options){
   gulp.task('helpers', function(){
@@ -15,15 +16,37 @@ module.exports = function(options){
       .pipe(gulp.dest(options.ngXosVendor));
   });
 
-  gulp.task('docs', function(){
-    return gulp.src(options.xosHelperSource + '**/*.js')
-      .pipe(gulpDocs.process({
-        title: 'XOS Helpers Module',
-        scripts: [
-          'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js',
-          'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js'
-        ]
-      }))
-      .pipe(gulp.dest('./docs'));
+  gulp.task('cleanDocs', function(){
+    console.log(options);
+    return del([options.docs + '**/*']);
+  });
+
+  gulp.task('docs', ['cleanDocs'], function(){
+    var ngOptions = {
+      scripts: [
+        'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js',
+        'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js'
+      ],
+      html5Mode: true,
+      title: 'XOS Helpers documentation',
+      startPage: '/module',
+    }
+
+    return gulpDocs.sections({
+      module: {
+        glob: [
+          options.xosHelperSource + '*.js',
+          options.xosHelperSource + 'services/*.js'
+        ],
+        title: 'Module Documentation',
+      },
+      'rest-api': {
+        glob: [
+          options.xosHelperSource + 'services/rest/*.js'
+        ],
+        api: true,
+        title: 'API Documentation',
+      }
+    }).pipe(gulpDocs.process(ngOptions)).pipe(gulp.dest('./docs'));
   });
 };
--- views/ngXosLib/xosHelpers/src/services/rest/vSG.js
+++ views/ngXosLib/xosHelpers/src/services/rest/vSG.js
@@ -2,6 +2,11 @@
   'use strict';
 
   angular.module('xos.helpers')
+  /**
+  * @ngdoc service
+  * @name xos.helpers.vSG-Collection
+  * @description Angular resource to fetch /api/service/vsg/
+  **/
   .service('vSG-Collection', function($resource){
     return $resource('/api/service/vsg/');
   })
