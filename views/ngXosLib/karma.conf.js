a7e64fd12008f053011e838ef27ab8b83c5a8ee5
Matteo Scandolo <teo@onlab.us>
2016-04-14 14:59:09 -0700

    Table component with action

--- views/ngXosLib/karma.conf.js
+++ views/ngXosLib/karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.test.js': ['babel']
+      'xosHelpers/**/*.js': ['babel'],
     },
 
     babelPreprocessor: {
