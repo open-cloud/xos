fda63ce3d76cb928a436cda909f7ebc38c1321fa
Jeremy Mowery <jermowery@email.arizona.edu>
2016-04-19 10:30:28 -0700

    Fix admin

--- xos/core/admin.py
+++ xos/core/admin.py
@@ -1,6 +1,8 @@
 import threading
 from cgi import escape as html_escape
 
+from core.models import *
+from core.models import Site
 from django import forms
 from django.contrib import admin, messages
 from django.contrib.admin.widgets import (AdminTextareaWidget,
@@ -19,9 +21,6 @@ from django.utils.encoding import force_text, python_2_unicode_compatible
 from django.utils.html import conditional_escape, format_html
 from django.utils.safestring import mark_safe
 from django.utils.text import capfirst
-
-from core.models import *
-from core.models import Site
 from openstack.manager import OpenStackManager
 from suit.widgets import LinkedSelect
 
