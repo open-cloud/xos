384c1ae27626aa13f742fb4b5469fca2783de695
Scott Baker <smbaker@gmail.com>
2016-04-20 21:32:57 -0700

    fix class name

--- xos/tosca/resources/vtnservice.py
+++ xos/tosca/resources/vtnservice.py
@@ -9,7 +9,7 @@ from services.vtn.models import VTNService
 
 from service import XOSService
 
-class XOSVRouterService(XOSService):
+class XOSVTNService(XOSService):
     provides = "tosca.nodes.VTNService"
     xos_model = VTNService
     copyin_props = ["view_url", "icon_url", "enabled", "published", "public_key", "versionNumber", 'privateGatewayMac', 'localManagementIp', 'ovsdbPort', 'sshPort', 'sshUser', 'sshKeyFile', 'mgmtSubnetBits']
