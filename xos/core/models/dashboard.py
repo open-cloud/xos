52f6156b15248c5a490fdcece4e6886a589c2707
Zack Williams <zdw@artisancomputer.com>
2016-04-21 15:08:57 -0700

    "null has no effect on ManyToManyField" per django 1.8

--- xos/core/models/dashboard.py
+++ xos/core/models/dashboard.py
@@ -10,7 +10,7 @@ class DashboardView(PlCoreBase):
     url = StrippedCharField(max_length=1024, help_text="URL of Dashboard")
     controllers = models.ManyToManyField(Controller, blank=True, related_name="dashboardviews", through='ControllerDashboardView')
     enabled = models.BooleanField(default=True)
-    deployments = models.ManyToManyField(Deployment, blank=True, null=True, related_name="dashboardviews", help_text="Deployments that should be included in this view")
+    deployments = models.ManyToManyField(Deployment, blank=True, related_name="dashboardviews", help_text="Deployments that should be included in this view")
 
     def __unicode__(self):  return u'%s' % (self.name)
 
