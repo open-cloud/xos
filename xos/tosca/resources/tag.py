6d52740556019d279bfc70d66b652fa32131fb80
Scott Baker <smbaker@gmail.com>
2016-04-26 10:48:58 -0700

    set service to tag

--- xos/tosca/resources/tag.py
+++ xos/tosca/resources/tag.py
@@ -6,7 +6,7 @@ sys.path.append("/opt/tosca")
 from translator.toscalib.tosca_template import ToscaTemplate
 from django.contrib.contenttypes.models import ContentType
 
-from core.models import Tag
+from core.models import Tag, Service
 
 from xosresource import XOSResource
 
@@ -28,6 +28,10 @@ class XOSTag(XOSResource):
             target_model = self.engine.name_to_xos_model(self.user, target_name)
             args["content_type"] = ContentType.objects.get_for_model(target_model)
             args["object_id"] = target_model.id
+
+        service_name = self.get_requirement("tosca.relationships.MemberOfService", throw_exception=throw_exception)
+        if service_name:
+            args["service"] = self.get_xos_object(Service, name=service_name)
 
         # To uniquely identify a Tag, we must know the object that it is attached
         # to as well as the name of the Tag.
