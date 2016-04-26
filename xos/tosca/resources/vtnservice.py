4e4afeca1fd4fbe7056a3f14e7fe9cd688e2054e
Andy Bavier <acb@cs.princeton.edu>
2016-04-26 16:29:06 -0400

    Use TOSCA to configure VTN

--- xos/tosca/resources/vtnservice.py
+++ xos/tosca/resources/vtnservice.py
@@ -12,5 +12,4 @@ from service import XOSService
 class XOSVTNService(XOSService):
     provides = "tosca.nodes.VTNService"
     xos_model = VTNService
-    copyin_props = ["view_url", "icon_url", "enabled", "published", "public_key", "versionNumber", 'privateGatewayMac', 'localManagementIp', 'ovsdbPort', 'sshPort', 'sshUser', 'sshKeyFile', 'mgmtSubnetBits']
-
+    copyin_props = ["view_url", "icon_url", "enabled", "published", "public_key", "versionNumber", 'privateGatewayMac', 'localManagementIp', 'ovsdbPort', 'sshPort', 'sshUser', 'sshKeyFile', 'mgmtSubnetBits', 'xosEndpoint', 'xosUser', 'xosPassword']
