71d7546a8291e898d408f42110a0d8daf245797e
arpiagariu <arpiagariu@gmail.com>
2016-05-19 10:42:18 -0700

    Fixed bub CORD-147

--- xos/core/templatetags/core_tags.py
+++ xos/core/templatetags/core_tags.py
@@ -8,8 +8,10 @@ register = template.Library()
 @register.inclusion_tag('admin/tags/dashboard_list.html', takes_context=True)
 def dashboard_list(context):
     request = context['request']
-    dashboards = request.user.get_dashboards()
-    customize = DashboardView.objects.filter(name="Customize")
-    #print >>sys.stderr, request.path
-    result_list = list(chain(dashboards, customize))
+    if request.user.is_authenticated():
+        dashboards = request.user.get_dashboards()
+        customize = DashboardView.objects.filter(name="Customize")
+        result_list = list(chain(dashboards, customize))
+    else:
+        result_list = []
     return {'dashboards': result_list, 'path': request.path}
--- xos/xos/admin_customize/templatetags/admin_modify.py
+++ xos/xos/admin_customize/templatetags/admin_modify.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.templatetags.admin_modify import *
 from django.contrib.admin.templatetags.admin_modify import submit_row as original_submit_row
-
+import random
 @register.inclusion_tag('admin/submit_line.html', takes_context=True)
 def submit_row(context):
     ctx = original_submit_row(context)
@@ -11,3 +11,11 @@ def submit_row(context):
         'custom_delete_url': context.get("custom_delete_url",None),
         })                                                                  
     return ctx 
+
+
+
+@register.simple_tag
+def random_str(a):
+    a = ["Thanks","Thanks for spending some quality time with the Web site today.", "Thanks for spending some quality time with the Web site today.", "Thanks for spending some quality time",
+ "Thanks for visiting the Web site today"]
+    return a[random.randint(0,4)]
