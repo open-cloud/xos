3d7fcc8262af6ba73a015504561bd5b0353da67b
Scott Baker <smbaker@gmail.com>
2016-05-24 09:44:09 -0700

    fix class name

--- xos/tosca/resources/voltservice.py
+++ xos/tosca/resources/voltservice.py
@@ -9,7 +9,7 @@ from services.cord.models import VOLTService
 
 from service import XOSService
 
-class XOSVTNService(XOSService):
+class XOSVOLTService(XOSService):
     provides = "tosca.nodes.VOLTService"
     xos_model = VOLTService
     copyin_props = ["view_url", "icon_url", "kind", "enabled", "published", "public_key", "private_key_fn", "versionNumber"]
