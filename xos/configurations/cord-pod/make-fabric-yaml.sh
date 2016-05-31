557cffd0c37ee3ebe58267c2864dc632a04595d2
Andy Bavier <acb@cs.princeton.edu>
2016-05-31 14:29:32 -0400

    Initial checkin for fabric synchronizer

---
+++ xos/configurations/cord-pod/make-fabric-yaml.sh
@@ -0,0 +1,72 @@
+FN=$SETUPDIR/vtn-external.yaml
+
+rm -f $FN
+
+cat >> $FN <<EOF
+tosca_definitions_version: tosca_simple_yaml_1_0
+
+imports:
+   - custom_types/xos.yaml
+
+description: generate fabric configuration
+
+topology_template:
+  node_templates:
+
+    service#ONOS_Fabric:
+      type: tosca.nodes.ONOSService
+      requirements:
+      properties:
+          kind: onos
+          view_url: /admin/onos/onosservice/\$id$/
+          no_container: true
+          rest_hostname: onos-fabric
+          replaces: service_ONOS_Fabric
+
+    service#fabric:
+      type: tosca.nodes.FabricService
+      properties:
+          view_url: /admin/fabric/fabricservice/$id$/
+          replaces: service_fabric
+
+
+EOF
+
+NODES=$( bash -c "source $SETUPDIR/admin-openrc.sh ; nova host-list" |grep compute|awk '{print $2}' )
+I=0
+for NODE in $NODES; do
+    echo $NODE
+    cat >> $FN <<EOF
+    $NODE:
+      type: tosca.nodes.Node
+
+    # Fabric location field for node $NODE
+    ${NODE}_location_tag:
+      type: tosca.nodes.Tag
+      properties:
+          name: location
+          value: of:0000000000000001/1
+      requirements:
+          - target:
+              node: $NODE
+              relationship: tosca.relationships.TagsObject
+          - service:
+              node: service#ONOS_Fabric
+              relationship: tosca.relationships.MemberOfService
+EOF
+done
+
+cat >> $FN <<EOF
+    Fabric_ONOS_app:
+      type: tosca.nodes.ONOSFabricApp
+      requirements:
+          - onos_tenant:
+              node: service#ONOS_Fabric
+              relationship: tosca.relationships.TenantOfService
+          - fabric_service:
+              node: service#fabric
+              relationship: tosca.relationships.UsedByService
+      properties:
+          dependencies:
+          autogenerate: fabric-network-cfg
+EOF
---
+++ xos/synchronizers/fabric/steps/sync_vsgtenant.py
@@ -0,0 +1,53 @@
+import os
+import base64
+from collections import defaultdict
+from django.db.models import F, Q
+from xos.config import Config
+from synchronizers.base.openstacksyncstep import OpenStackSyncStep
+from synchronizers.base.syncstep import *
+from core.models import Controller
+from core.models import Image, ControllerImages
+from xos.logger import observer_logger as logger
+from synchronizers.base.ansible import *
+import json
+
+class SyncVSGTenant(SyncStep):
+    provides=[VSGTenant]
+    observes = VSGTenant
+    requested_interval=30
+    playbook='sync_vsgtenant.yaml'
+
+    def fetch_pending(self, deleted):
+        if (not deleted):
+            objs = VSGTenant.get_tenant_objects().filter(Q(lazy_blocked=False))
+        else:
+            objs = VSGTenant.get_deleted_tenant_objects()
+
+        return objs
+
+    def map_sync_inputs(self, vsgtenant):
+
+        rest_hostname = "onos-fabric"
+        rest_port = 8181
+
+        wan_ip = vsgtenant.wan_container_ip
+        wan_mac = vsgtenant.wan_container_mac
+
+        # Look up location - it's tagged on the nodes
+
+        # Figure out: is it a POST or DELETE?
+
+        # Create JSON
+        rest_json = ""
+
+        image_fields = {
+                        'rest_hostname': rest_hostname,
+                        'rest_port': rest_port,
+                        'rest_json': rest_json,
+                        'ansible_tag': '%s@%s'%(vsgtenant.name), # name of ansible playbook
+                        }
+
+	return image_fields
+
+    def map_sync_outputs(self, controller_image, res):
+        pass
