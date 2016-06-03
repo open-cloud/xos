9d0e0c779ba5b98f2c3bdaa01bb6bb3c2d6babbf
Andy Bavier <acb@cs.princeton.edu>
2016-06-03 15:56:32 -0400

    Try again

--- xos/services/fabric/models.py
+++ xos/services/fabric/models.py
@@ -1,23 +1,9 @@
 from django.db import models
-from core.models import Service, PlCoreBase, Slice, Instance, Tenant, TenantWithContainer, Node, Image, User, Flavor, Subscriber, NetworkParameter, NetworkParameterType, Port, AddressPool
-from core.models.plcorebase import StrippedCharField
-import os
-from django.db import models, transaction
-from django.forms.models import model_to_dict
-from django.db.models import Q
-from operator import itemgetter, attrgetter, methodcaller
-from core.models import Tag
-from core.models.service import LeastLoadedNodeScheduler
 import traceback
 from xos.exceptions import *
 from xos.config import Config
 
-class ConfigurationError(Exception):
-    pass
-
 FABRIC_KIND = "fabric"
-
-CORD_USE_VTN = getattr(Config(), "networking_use_vtn", False)
 
 class FabricService(Service):
     KIND = FABRIC_KIND
@@ -27,6 +13,4 @@ class FabricService(Service):
         verbose_name = "Fabric Service"
         proxy = True
 
-    simple_attributes = ( ("autoconfig", True) )
-
-FabricService.setup_simple_attributes()
+    autoconfig = models.BooleanField(default=True, help_text="Autoconfigure the fabric")
