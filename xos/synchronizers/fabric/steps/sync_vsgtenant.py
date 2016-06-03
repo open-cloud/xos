070863e7c6a5cf74b669fe24d60f92c7ea326c36
Andy Bavier <acb@cs.princeton.edu>
2016-06-03 11:41:06 -0400

    Add debug stmt

--- xos/synchronizers/fabric/steps/sync_vsgtenant.py
+++ xos/synchronizers/fabric/steps/sync_vsgtenant.py
@@ -67,6 +67,8 @@ class SyncVSGTenant(SyncStep):
 
         rest_json = json.dumps(data, indent=4)
 
+        logger.error("rest_hostname is %s" % fos.rest_hostname)
+
         fields = {
             'rest_hostname': fos.rest_hostname,
             'rest_port': fos.rest_port,
