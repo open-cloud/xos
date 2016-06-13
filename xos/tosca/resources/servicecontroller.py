a33af70b9d8db318194f327370ab8725f9808105
Scott Baker <smbaker@gmail.com>
2016-06-13 15:46:18 -0700

    fix error

--- xos/tosca/resources/servicecontroller.py
+++ xos/tosca/resources/servicecontroller.py
@@ -29,7 +29,7 @@ class XOSServiceController(XOSResource):
                     parts=value.split()
                     for part in parts[:-1]:
                        if ":" in part:
-                           (lhs, rhs) = part.split(1)
+                           (lhs, rhs) = part.split(":", 1)
                            if lhs=="subdirectory":
                                subdirectory=rhs
                            else:
