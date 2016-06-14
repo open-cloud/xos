5c88974e4b884f3b30251c0b12fccb53e70f35c7
Scott Baker <smbaker@gmail.com>
2016-06-14 11:45:55 -0700

    remove fabric from settings.py and xos-manage

--- xos/xos/settings.py
+++ xos/xos/settings.py
@@ -191,7 +191,7 @@ INSTALLED_APPS = (
 #    'services.vtr',
     'services.vrouter',
 #    'services.vtn',
-    'services.fabric',
+#    'services.fabric',
     'geoposition',
     'rest_framework_swagger',
 )
