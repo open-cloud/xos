6a6aa797b780c25434e396a62bdb2df450ee7959
Scott Baker <smbaker@gmail.com>
2016-08-10 10:08:15 -0700

    remove hpc synchronizer
    Change-Id: I5176b50b4cae7479f653f98408034ecad340d977

--- xos/synchronizers/hpc/run.sh
+++
@@ -1,2 +0,0 @@
-export XOS_DIR=/opt/xos
-python hpc-synchronizer.py  -C $XOS_DIR/synchronizers/hpc/hpc_synchronizer_config
--- xos/synchronizers/hpc/start.sh
+++
@@ -1,2 +0,0 @@
-export XOS_DIR=/opt/xos
-nohup python hpc-synchronizer.py  -C $XOS_DIR/synchronizers/hpc/hpc_synchronizer_config > /dev/null 2>&1 &
