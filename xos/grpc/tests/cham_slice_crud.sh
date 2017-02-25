d04ee96e63be901ed3979875288026e8ae69e0f6
Scott Baker <smbaker@gmail.com>
2017-02-24 16:55:07 -0800

    CORD-866 fix chameleon crud test
    Change-Id: I227fb8ef2a9fa9d00c4344e0b92f1637cde18c75

--- xos/grpc/tests/cham_slice_crud.sh
+++ xos/grpc/tests/cham_slice_crud.sh
@@ -1,6 +1,6 @@
 source /opt/xos/grpc/tests/testconfig-chameleon.sh
 
-RESPONSE=`curl -X POST -H "Content-Type: application/json" -d "{\"username\": \"$USERNAME\", \"$PASSWORD\"}" http://$HOSTNAME:8080/xosapi/v1/utility/login`
+RESPONSE=`curl -X POST -H "Content-Type: application/json" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" http://$HOSTNAME:8080/xosapi/v1/utility/login`
 SESSIONID=`echo $RESPONSE | python -c "import json,sys; print json.load(sys.stdin)['sessionid']"`
 echo "sessionid=$SESSIONID"
 
@@ -9,14 +9,14 @@ SLICENAME="mysite_$RS"
 
 echo "slicename=$SLICENAME"
 
-RESPONSE=`curl -X POST -H "x-xossession: $SESSIONID" -H "Content-Type: application/json" -d "{\"name\": \"$SLICENAME\", \"site_id\": 1}" http://$HOSTNAME:8080/xosapi/v1/slices`
+RESPONSE=`curl -X POST -H "x-xossession: $SESSIONID" -H "Content-Type: application/json" -d "{\"name\": \"$SLICENAME\", \"site_id\": 1}" http://$HOSTNAME:8080/xosapi/v1/core/slices`
 
 echo "create response: $RESPONSE"
 SLICEID=`echo $RESPONSE | python -c "import json,sys; print json.load(sys.stdin)['id']"`
 
-RESPONSE=`curl -X GET -H "x-xossession: $SESSIONID" http://$HOSTNAME:8080/xosapi/v1/slices/$SLICEID`
+RESPONSE=`curl -X GET -H "x-xossession: $SESSIONID" http://$HOSTNAME:8080/xosapi/v1/core/slices/$SLICEID`
 echo "get response: $RESPONSE"
 
-RESPONSE=`curl -X DELETE -H "x-xossession: $SESSIONID" http://$HOSTNAME:8080/xosapi/v1/slices/$SLICEID`
+RESPONSE=`curl -X DELETE -H "x-xossession: $SESSIONID" http://$HOSTNAME:8080/xosapi/v1/core/slices/$SLICEID`
 
 echo "delete response: $RESPONSE"
