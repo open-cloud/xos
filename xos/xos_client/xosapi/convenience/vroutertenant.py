bb3aaafdb79e00102219af0ff2bdec994cc531aa
Scott Baker <smbaker@gmail.com>
2017-03-13 12:12:17 -0700

    CORD-879 update vrouter wrapper to reflect real object
    Change-Id: I7286bc7ed47e42349bc3b81e21e80a5d44ff20b3

--- xos/xos_client/xosapi/convenience/vroutertenant.py
+++ xos/xos_client/xosapi/convenience/vroutertenant.py
@@ -2,27 +2,6 @@ import json
 from xosapi.orm import ORMWrapper, register_convenience_wrapper
 
 class ORMWrapperVRouterTenant(ORMWrapper):
-    # hopefully this goes away when VRouterTenant is made a real object
-
-    def get_attribute(self, name, default=None):
-        if self.service_specific_attribute:
-            attributes = json.loads(self.service_specific_attribute)
-        else:
-            attributes = {}
-        return attributes.get(name, default)
-
-    @property
-    def address_pool_id(self):
-        return self.get_attribute("address_pool_id", None)
-
-    @property
-    def public_ip(self):
-        return self.get_attribute("public_ip", None)
-
-    @property
-    def public_mac(self):
-        return self.get_attribute("public_mac", None)
-
     @property
     def gateway_ip(self):
         if not self.address_pool:
@@ -50,14 +29,5 @@ class ORMWrapperVRouterTenant(ORMWrapper):
                 return int(parts[1].strip())
         return None
 
-    @property
-    def address_pool(self):
-        if not self.address_pool_id:
-            return None
-        aps = self.stub.AddressPool.objects.filter(id=self.address_pool_id)
-        if not aps:
-            return None
-        ap = aps[0]
-        return ap
 
 register_convenience_wrapper("VRouterTenant", ORMWrapperVRouterTenant)
