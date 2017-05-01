6f9cfb8fd46767473a8753cc5d856827cf3a857c
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-05-01 15:36:37 -0700

    [CORD-967] Returning errors in JSON
    Change-Id: I35f48f6a8ec1931b1242a78f6777fffc184f54df

--- xos/xos/exceptions.py
+++ xos/xos/exceptions.py
@@ -1,71 +1,138 @@
+import json
 from rest_framework.exceptions import APIException
 from rest_framework.exceptions import PermissionDenied as RestFrameworkPermissionDenied
+
+def _get_json_error_details(data):
+    """
+    Convert error details to JSON
+    """
+    if isinstance(data, dict):
+        ret = {
+            key: value for key, value in data.items()
+        }
+    elif isinstance(data, list):
+        ret = [
+            item for item in data
+        ]
+
+    return json.dumps(ret)
+
 
 class XOSProgrammingError(APIException):
     status_code=400
     def __init__(self, why="programming error", fields={}):
-        APIException.__init__(self, {"error": "XOSProgrammingError",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSProgrammingError",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSPermissionDenied(RestFrameworkPermissionDenied):
     def __init__(self, why="permission error", fields={}):
-        APIException.__init__(self, {"error": "XOSPermissionDenied",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSPermissionDenied",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSNotAuthenticated(RestFrameworkPermissionDenied):
     status_code=401
     def __init__(self, why="you must be authenticated to use this api", fields={}):
-        APIException.__init__(self, {"error": "XOSNotAuthenticated",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSNotAuthenticated",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSNotFound(RestFrameworkPermissionDenied):
     status_code=404
     def __init__(self, why="object not found", fields={}):
-        APIException.__init__(self, {"error": "XOSNotFound",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSNotFound",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSValidationError(APIException):
     status_code=403
     def __init__(self, why="validation error", fields={}):
-        APIException.__init__(self, {"error": "XOSValidationError",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSValidationError",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSDuplicateKey(APIException):
     status_code=400
     def __init__(self, why="duplicate key", fields={}):
-        APIException.__init__(self, {"error": "XOSDuplicateKey",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSDuplicateKey",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSMissingField(APIException):
     status_code=400
     def __init__(self, why="missing field", fields={}):
-        APIException.__init__(self, {"error": "XOSMissingField",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSMissingField",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSConfigurationError(APIException):
     status_code=400
     def __init__(self, why="configuration error", fields={}):
-        APIException.__init__(self, {"error": "XOSConfigurationError",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSConfigurationError",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSConflictingField(APIException):
     status_code=400
     def __init__(self, why="conflicting field", fields={}):
-        APIException.__init__(self, {"error": "XOSMissingField",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSMissingField",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
 
 class XOSServiceUnavailable(APIException):
     status_code=503
     def __init__(self, why="Service temporarily unavailable, try again later", fields={}):
-        APIException.__init__(self, {"error": "XOSServiceUnavailable",
-                            "specific_error": why,
-                            "fields": fields})
+        raw_detail = {
+            "error": "XOSServiceUnavailable",
+            "specific_error": why,
+            "fields": fields
+        }
+        APIException.__init__(self, raw_detail)
+        self.raw_detail = raw_detail
+        self.json_detail = _get_json_error_details(raw_detail)
