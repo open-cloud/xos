443d4634b3df1b32cef3cdd4e035ede02b3f01ad
shivani vaidya <shivani@onlab.us>
2017-05-08 14:17:38 -0700

    Adding error messages to slice_model
    Change-Id: I750ec67cfd704dc17a89218389bdf210aaade0e9

--- xos/core/models/attic/slice_model.py
+++ xos/core/models/attic/slice_model.py
@@ -8,13 +8,13 @@ def save(self, *args, **kwds):
     site = Site.objects.get(id=self.site.id)
     # allow preexisting slices to keep their original name for now
     if not self.id and not self.name.startswith(site.login_base):
-        raise XOSValidationError('slice name must begin with %s' % site.login_base)
+        raise XOSValidationError('slice name must begin with %s' % site.login_base, {'name' : 'slice name must begin with %s' % site.login_base})
 
     if self.name == site.login_base+"_":
-        raise XOSValidationError('slice name is too short')
+        raise XOSValidationError('slice name is too short', {'name': 'slice name is too short'})
 
     if " " in self.name:
-        raise XOSValidationError('slice name must not contain spaces')
+        raise XOSValidationError('slice name must not contain spaces', {'name': 'slice name must not contain spaces'})
 
     # set creator on first save
     if not self.creator and hasattr(self, 'caller'):
@@ -29,10 +29,11 @@ def save(self, *args, **kwds):
             # himeself on a new slice object.
             pass
         else:
-            raise PermissionDenied("Insufficient privileges to change slice creator")
+            raise PermissionDenied("Insufficient privileges to change slice creator",
+                                   {'creator': "Insufficient privileges to change slice creator"})
     
     if not self.creator:
-        raise XOSValidationError('slice has no creator')
+        raise XOSValidationError('slice has no creator', {'creator': 'slice has no creator'})
 
     if self.network=="Private Only":
         # "Private Only" was the default from the old Tenant View
