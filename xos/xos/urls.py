1aa2eae677c5afd1b5f411b68f6fcdde04d8aa0b
Scott Baker <smbaker@gmail.com>
2017-08-13 10:17:54 -0700

    CORD-1286 remove legacy admin and API
    Change-Id: I32c95d6b82357960c6897aa2a29a9ce179f91405

--- xos/xos/urls.py
+++ xos/xos/urls.py
@@ -17,22 +17,9 @@
 import importlib
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-
-# This is the generated API
-from xosapi import *
-
-# from services.vbbu.view import *
-from core.views.mcordview import *
-# from core.views.analytics import AnalyticsAjaxView
-
 from core.models import *
 from rest_framework import generics
-from core.dashboard.sites import SitePlus
 from django.http import HttpResponseRedirect
-
-# from core.xoslib import XOSLibDataView
 
 def load_class(full_class_string):
     """
@@ -47,12 +34,6 @@ def load_class(full_class_string):
     # Finally, we retrieve the Class
     return getattr(module, class_str)
 
-# from api import import_api_methods
-
-admin.site = SitePlus()
-admin.autodiscover()
-
-
 def redirect_to_apache(request):
     """ bounce a request back to the apache server that is running on the machine """
     apache_url = "http://%s%s" % (request.META['HOSTNAME'], request.path)
@@ -60,29 +41,14 @@ def redirect_to_apache(request):
 
 urlpatterns = patterns(
     '',
-
-    # url(r'^docs/', include('rest_framework_swagger.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(admin.site.urls)),
-    # url(r'^profile/home', 'core.views.home'),
-
-    # url(r'^admin/xoslib/(?P<name>\w+)/$', XOSLibDataView.as_view(), name="xoslib"),
-
-    # url(r'^analytics/(?P<name>\w+)/$', AnalyticsAjaxView.as_view(), name="analytics"),
-
+    # Is this necessary?
     url(r'^files/', redirect_to_apache),
 
     # Adding in rest_framework urls
     url(r'^xos/', include('rest_framework.urls', namespace='rest_framework')),
 
-    # XOSLib rest methods [deprecated]
-    url(r'^xoslib/', include('core.xoslib.methods', namespace='xoslib')),
-
+    # handcrafted API methods
+    # NOTE: Needs to stay until Tosca switchover is complete
     url(r'^', include('api.import_methods', namespace='api')),
+  )
 
-  ) + get_REST_patterns()
