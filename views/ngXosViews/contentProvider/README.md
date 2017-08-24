1e567f581ec43ec4ce536b0ee858e235e08df93c
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-08-24 13:28:02 -0700

    [CORD-1333] Removing ngXosLib
    Change-Id: I5ff3cdd090c63472da6146649129b426d4b3608d

--- views/ngXosViews/contentProvider/README.md
+++
@@ -1,9 +0,0 @@
-# Content Provider Dashboard
-
-To enable the needed backend for this dashboard:
-
-- Enter `xos` with `make enter-xos` (from `xos/configuration/frontend` folder)
-- `cd /opt/xos/tosca`
-- `python ./run.py padmin@vicci.org samples/cdn.yaml`
-
-And it will enable the `xoslib/hpcapi` endpoint
--- xos/core/models/user.py
+++ xos/core/models/user.py
@@ -23,8 +23,7 @@ from collections import defaultdict
 from operator import attrgetter, itemgetter
 
 from core.middleware import get_request
-from xosbase import XOSBase,PlModelMixIn
-import dashboardview
+from xosbase import XOSBase, PlModelMixIn
 from core.models.xosbase import StrippedCharField, XOSCollector
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.exceptions import PermissionDenied
@@ -165,9 +164,7 @@ class User(AbstractBaseUser, PlModelMixIn):
     no_policy = models.BooleanField(default=False)   # prevent model_policy run
 
     timezone = TimeZoneField()
-
-    dashboards = models.ManyToManyField(
-        'DashboardView', through='UserDashboardView', blank=True)
+    
     leaf_model_name = models.CharField( help_text = "The most specialized model in this chain of inheritance, often defined by a service developer", max_length = 1024, null = False )
 
     policy_status = models.CharField( default = "0 - Policy in process", max_length = 1024, null = True )
@@ -217,21 +214,6 @@ class User(AbstractBaseUser, PlModelMixIn):
 
     def is_superuser(self):
         return False
-
-    def get_dashboards(self):
-        DEFAULT_DASHBOARDS = ["Tenant"]
-
-        dashboards = sorted(
-            list(self.userdashboardviews.all()), key=attrgetter('order'))
-        dashboards = [x.dashboardView for x in dashboards]
-
-        if (not dashboards) and (not self.is_appuser):
-            for dashboardName in DEFAULT_DASHBOARDS:
-                dbv = dashboardview.DashboardView.objects.filter(name=dashboardName)
-                if dbv:
-                    dashboards.append(dbv[0])
-
-        return dashboards
 
 #    def get_roles(self):
 #        from core.models.site import SitePrivilege
@@ -352,8 +334,6 @@ class User(AbstractBaseUser, PlModelMixIn):
         elif profile == "cp":
             self.is_appuser = True
             self.is_admin = False
-            for db in self.userdashboardviews.all():
-                db.delete()
 
     def get_content_type_key(self):
         ct = ContentType.objects.get_for_model(self.__class__)
@@ -374,10 +354,4 @@ class User(AbstractBaseUser, PlModelMixIn):
     generate the User class'''
     def can_access(self, ctx):
         return security.user_policy_security_check(self, ctx), "user_policy"
-
-class UserDashboardView(XOSBase):
-    user = models.ForeignKey(User, related_name='userdashboardviews')
-    dashboardView = models.ForeignKey(
-        dashboardview.DashboardView, related_name='userdashboardviews')
-    order = models.IntegerField(default=0)
 
