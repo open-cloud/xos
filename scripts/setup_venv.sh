996386f56b0b42c61be68116be5142ae765f93ac
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-09-14 13:44:33 -0700

    Moving - to _
    Change-Id: I1f66df5b3b40dfa44b3a5df40ee7c683bb097f45
    (cherry picked from commit 9b849eb1ee572089800fda87d9cb26b73d8d13eb)

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -24,16 +24,16 @@ echo $VENVDIR
 
 # create venv if it's not yet there
 if [ ! -d "$BASEDIR/$VENVDIR" ]; then
-    echo "Setting up virtualenv for XOS"
-    virtualenv -q $BASEDIR/$VENVDIR --no-site-packages
-    pip install --upgrade pip
-    echo "Virtualenv created."
+   echo "Setting up virtualenv for XOS"
+   virtualenv -q $BASEDIR/$VENVDIR --no-site-packages
+   pip install --upgrade pip
+   echo "Virtualenv created."
 fi
 
 # activate the virtual env
 if [ ! $VIRTUAL_ENV ]; then
-    source $BASEDIR/$VENVDIR/bin/activate
-    echo "Virtualenv activated."
+   source $BASEDIR/$VENVDIR/bin/activate
+   echo "Virtualenv activated."
 fi
 
 # install pip requirements
@@ -49,10 +49,10 @@ cd $BASEDIR/xos/xos_client; python setup.py install && \
 chmod 777 $BASEDIR/venv-xos/lib/python2.7/site-packages/xosapi/chameleon/protoc_plugins/gw_gen.py && \
 chmod 777 $BASEDIR/venv-xos/lib/python2.7/site-packages/xosapi/chameleon/protoc_plugins/swagger_gen.py && \
 cd $BASEDIR/lib/xos-genx; python setup.py install
-  then
-    echo "Requirements installed."
-    echo "Virtualenv ready"
-  else
-    echo "An error occurred"
+ then
+   echo "Requirements installed."
+   echo "Virtualenv ready"
+ else
+   echo "An error occurred"
 fi
 cd $BASEDIR
