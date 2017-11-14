0fef00985fe992436034607bcaa96ed896659bdd
Scott Baker <smbaker@gmail.com>
2017-11-14 13:57:18 -0800

    CORD-1993 port model_policy test to new framework; cherry-picked from gerrit 6998 commit d8b1439d9e6e1392546a2c26aaccbdf6f92c7b25
    Change-Id: I387bd16c3ae2c863e9a0fb7d5dcdac731779b5b9

--- xos/synchronizers/new_base/model_policies/test_model_policy_tenantwithcontainer.py
+++ xos/synchronizers/new_base/model_policies/test_model_policy_tenantwithcontainer.py
@@ -22,95 +22,64 @@ import pdb
 import os, sys
 from xosconfig import Config
 
-TEST_PATH=os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(os.path.join(TEST_PATH, "../../.."))
-sys.path.append(os.path.join(TEST_PATH, "../../new_base/model_policies"))
-
-class MockObject:
-    objects = None
-    def __init__(self, **kwargs):
-        for (k,v) in kwargs.items():
-            setattr(self,k,v)
-    def save(self):
-        pass
-    def delete(self):
-        pass
-
-class MockFlavor(MockObject):
-    name = None
-
-class MockInstance(MockObject):
-    name = None
-
-class MockDeployment(MockObject):
-    name = None
-
-class MockUser(MockObject):
-    email = None
-
-class MockSlice(MockObject):
-    name = None
-
-class MockNode(MockObject):
-    hostname = None
-    site_deployment = None
-
-class MockImage(MockObject):
-    name = None
-
-class MockTenant(MockObject):
-    owner = None
-    deleted = False
-    instance = None
-    creator = None
-    service_specific_attribute = {}
-
-    def get_image(self):
-        return None
+test_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+xos_dir = os.path.join(test_path, '..', '..', '..')
 
 class TestModelPolicyTenantWithContainer(unittest.TestCase):
     def setUp(self):
         global TenantWithContainerPolicy, LeastLoadedNodeScheduler
 
+        self.sys_path_save = sys.path
+        self.cwd_save = os.getcwd()
+        sys.path.append(xos_dir)
+        #sys.path.append(os.path.join(xos_dir, 'synchronizers', 'new_base'))
+        sys.path.append(os.path.join(xos_dir, 'synchronizers', 'new_base', 'model_policies'))
+
         config = basic_conf = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/test_config.yaml")
         Config.clear() # in case left unclean by a previous test case
         Config.init(config, 'synchronizer-config-schema.yaml')
-        import synchronizers.new_base.modelaccessor
         import model_policy_tenantwithcontainer
         from model_policy_tenantwithcontainer import TenantWithContainerPolicy, LeastLoadedNodeScheduler
 
+        # import all class names to globals
+        for (k, v) in model_policy_tenantwithcontainer.model_accessor.all_model_classes.items():
+            globals()[k] = v
+
+        # TODO: Mock_model_accessor lacks save or delete methods
+        #Instance.save = mock.Mock
+        #Instance.delete = mock.Mock
+        #TenantWithContainer.save = mock.Mock
+
         self.policy = TenantWithContainerPolicy()
-        self.user = MockUser(email="testadmin@test.org")
-        self.tenant = MockTenant(creator=self.user)
-        self.flavor = MockFlavor(name="m1.small")
-        model_policy_tenantwithcontainer.Instance = MockInstance
-        model_policy_tenantwithcontainer.Flavor = MockFlavor
+        self.user = User(email="testadmin@test.org")
+        self.tenant = TenantWithContainer(creator=self.user)
+        self.flavor = Flavor(name="m1.small")
 
     def tearDown(self):
         Config.clear()
+        sys.path = self.sys_path_save
+        os.chdir(self.cwd_save)
 
-    @patch.object(MockTenant, "owner")
-    def test_manage_container_no_slices(self, owner):
-        owner.slices.count.return_value = 0
-        with self.assertRaises(Exception) as e:
-            self.policy.manage_container(self.tenant)
-        self.assertEqual(e.exception.message, "The service has no slices")
+    def test_manage_container_no_slices(self):
+        with patch.object(TenantWithContainer, "owner") as owner:
+            owner.slices.count.return_value = 0
+            with self.assertRaises(Exception) as e:
+                self.policy.manage_container(self.tenant)
+            self.assertEqual(e.exception.message, "The service has no slices")
 
-    @patch.object(MockTenant, "owner")
-    @patch.object(MockTenant, "save")
-    #@patch.object(TenantWithContainerPolicy, "get_image")
-    #@patch.object(LeastLoadedNodeScheduler, "pick")
-    @patch.object(MockNode, "site_deployment")
-    @patch.object(MockInstance, "save")
-    @patch.object(MockInstance, "delete")
-    def test_manage_container(self, instance_delete, instance_save, site_deployment, tenant_save, owner):
-      with patch.object(TenantWithContainerPolicy, "get_image") as get_image, \
+    def test_manage_container(self):
+      with patch.object(TenantWithContainer, "owner") as owner, \
+           patch.object(TenantWithContainer, "save") as tenant_save, \
+           patch.object(Node, "site_deployment") as site_deployment, \
+           patch.object(Instance, "save") as instance_save, \
+           patch.object(Instance, "delete") as instance_delete, \
+           patch.object(TenantWithContainerPolicy, "get_image") as get_image, \
            patch.object(LeastLoadedNodeScheduler, "pick") as pick:
         # setup mocks
-        node = MockNode(hostname="my.node.com")
-        slice = MockSlice(name="mysite_test1", default_flavor=self.flavor, default_isolation="vm")
-        image = MockImage(name="trusty-server-multi-nic")
-        deployment = MockDeployment(name="testdeployment")
+        node = Node(hostname="my.node.com")
+        slice = Slice(name="mysite_test1", default_flavor=self.flavor, default_isolation="vm")
+        image = Image(name="trusty-server-multi-nic")
+        deployment = Deployment(name="testdeployment")
         owner.slices.count.return_value = 1
         owner.slices.all.return_value = [slice]
         owner.slices.first.return_value = slice
@@ -144,22 +113,17 @@ class TestModelPolicyTenantWithContainer(unittest.TestCase):
         # make sure manage_container did what it is supposed to do
         self.assertEqual(self.tenant.instance, None)
 
-    @patch.object(MockTenant, "owner")
-    @patch.object(MockTenant, "save")
-    #@patch.object(TenantWithContainerPolicy, "get_image")
-    #@patch.object(LeastLoadedNodeScheduler, "pick")
-    @patch.object(MockNode, "site_deployment")
-    @patch.object(MockInstance, "save")
-    @patch.object(MockInstance, "delete")
-    @patch.object(MockFlavor, "objects")
-    def test_manage_container_no_m1_small(self, flavor_objects, instance_delete, instance_save, site_deployment, tenant_save, owner):
-      with patch.object(TenantWithContainerPolicy, "get_image") as get_image, \
+    def test_manage_container_no_m1_small(self):
+      with patch.object(TenantWithContainer, "owner") as owner, \
+           patch.object(Node, "site_deployment") as site_deployment, \
+           patch.object(Flavor, "objects") as flavor_objects, \
+           patch.object(TenantWithContainerPolicy, "get_image") as get_image, \
                 patch.object(LeastLoadedNodeScheduler, "pick") as pick:
         # setup mocks
-        node = MockNode(hostname="my.node.com")
-        slice = MockSlice(name="mysite_test1", default_flavor=None, default_isolation="vm")
-        image = MockImage(name="trusty-server-multi-nic")
-        deployment = MockDeployment(name="testdeployment")
+        node = Node(hostname="my.node.com")
+        slice = Slice(name="mysite_test1", default_flavor=None, default_isolation="vm")
+        image = Image(name="trusty-server-multi-nic")
+        deployment = Deployment(name="testdeployment")
         owner.slices.count.return_value = 1
         owner.slices.all.return_value = [slice]
         owner.slices.first.return_value = slice
