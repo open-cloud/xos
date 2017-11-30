5103bc3ff0d02f05fb185b5b0417deb19314483b
Sapan Bhatia <gwsapan@gmail.com>
2017-11-29 19:07:59 -0500

    Prevent multiple objects from sharing the same sync step
    Change-Id: I82d546df942aa1e2c320c9d91d10506381990a1f

--- xos/synchronizers/new_base/event_loop.py
+++ xos/synchronizers/new_base/event_loop.py
@@ -457,6 +457,8 @@ class XOSObserver:
             if not hasattr(step, 'call'):
                 pending = step.fetch_pending(deletion)
                 for obj in pending:
+                    step = step_class(driver=self.driver)
+                    step.log = self.log.bind(step=step)
                     obj.synchronizer_step = step
 
                 pending_service_dependencies = self.compute_service_instance_dependencies(
