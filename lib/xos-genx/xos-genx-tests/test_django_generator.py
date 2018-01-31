dfceafa5769b6be249a3c0cf8b78d194cc275ea3
Matteo Scandolo <teo@opennetworking.org>
2018-01-30 17:44:23 -0800

    [CORD-2671] Adding test for Django relations
    Change-Id: I438746fbbcb51762c2809cd94710dc3a2f4c90db

--- lib/xos-genx/xos-genx-tests/test_django_generator.py
+++ lib/xos-genx/xos-genx-tests/test_django_generator.py
@@ -37,6 +37,42 @@ class XProtoProtobufGeneratorTest(unittest.TestCase):
         links = filter(lambda s:'Key(' in s, output.splitlines())
         self.assertEqual(len(links), 2)
 
+    def test_optional_relations(self):
+        """
+        [XOS-GenX] Generate DJANGO models, verify relations
+        """
+        xproto = \
+            """
+            option app_label = "test";
+
+            message ENodeB {
+            }
+            
+            message Handover {
+            }
+
+            message Foo {
+                optional manytoone enodeb->ENodeB:profiles = 1 [null = True, blank = True];
+                required manytoone handover->Handover:profiles = 2 [null = False, blank = False];
+            }
+            """
+
+        args = FakeArgs()
+        args.inputs = xproto
+        args.target = 'django.xtarget'
+        output = XOSGenerator.generate(args)
+
+        null_true = filter(lambda s: 'null = True' in s, output.splitlines())
+        null_false = filter(lambda s: 'null = False' in s, output.splitlines())
+
+        blank_true = filter(lambda s: 'blank = True' in s, output.splitlines())
+        blank_false = filter(lambda s: 'blank = False' in s, output.splitlines())
+
+        self.assertEqual(len(null_true), 1)
+        self.assertEqual(len(null_false), 1)
+        self.assertEqual(len(blank_true), 1)
+        self.assertEqual(len(blank_false), 1)
+
 if __name__ == '__main__':
     unittest.main()
 
